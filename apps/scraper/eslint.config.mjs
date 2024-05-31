a2e88bd0236ee0f9a09a5b1b72b6d173eb8b66a8
Raman Gupta <gr.gupta.raman@gmail.com>
2024-05-31 06:17:20 -0400

    feat: setup basic project project with typescript, prettier, and eslint

---
+++ apps/scraper/eslint.config.mjs
@@ -0,0 +1,12 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default [
+  { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+  pluginJs.configs.recommended,
+  prettier,
+  ...tseslint.configs.recommended,
+];
+
