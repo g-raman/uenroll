56d55cb09a72d55050613f79f5e98b858c645da5
Raman Gupta <gr.gupta.raman@gmail.com>
2024-06-02 17:10:56 -0400

    feat: fetch list of available courses and terms

--- apps/scraper/src/index.ts
+++ apps/scraper/src/index.ts
@@ -1,10 +1,21 @@
-import { configDotenv } from 'dotenv';
 import puppeteer, { Browser, Page } from 'puppeteer';
 import { URL } from './constants';
-
-configDotenv({ path: './config.env' });
+import supabase from './supabase';
 
 async function main() {
+  const coursesResult = await supabase.from('availableCourses').select('subject').order('subject', { ascending: true });
+  if (coursesResult.error) {
+    console.error('Error: Something went wrong when fetching list of available courses');
+    console.log(coursesResult.error);
+  }
+  const courses = coursesResult?.data?.map((course) => course.subject);
+
+  const terms = await supabase.from('availableTerms').select('term,value').order('term', { ascending: true });
+  if (terms.error) {
+    console.error('Error: Something went wrong when fetching list of available terms');
+    console.log(terms.error);
+  }
+
   const browser: Browser = await puppeteer.launch({ headless: false });
   const page: Page = await browser.newPage();
   await page.goto(URL, { waitUntil: 'networkidle0' });
