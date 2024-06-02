401f652e5024493e4c20f2fdb862348b07b290aa
Raman Gupta <gr.gupta.raman@gmail.com>
2024-06-02 16:39:03 -0400

    feat: save new available courses to database

--- apps/scraper/src/scrapeAvailableCourses.ts
+++ apps/scraper/src/scrapeAvailableCourses.ts
@@ -1,5 +1,31 @@
 import puppeteer, { Browser, Page, Protocol } from 'puppeteer';
 import { URL } from './constants';
+import { createClient } from '@supabase/supabase-js';
+import { configDotenv } from 'dotenv';
+
+configDotenv({ path: 'src/config.env' });
+
+const supabaseUrl = process.env.SUPABASE_URL as string;
+const supabaseKey = process.env.SUPABASE_KEY as string;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+type subject = {
+  subject: string;
+};
+
+async function updateAvailableCourses(availableCourses: subject[]): Promise<void> {
+  console.log('Inserting new available courses...');
+  const { error } = await supabase
+    .from('availableCourses')
+    .upsert(availableCourses, { onConflict: 'subject' })
+    .select();
+  if (error) {
+    console.error('Error: Something went wrong when inserting new available courses data');
+    console.log(error);
+    return;
+  }
+  console.log('Successfully inserted new available courses');
+}
 
 function extractCourseStartingLetter(selector: string): string {
   const regex = /(.)(?=\.PSPAGE)/;
@@ -15,7 +41,7 @@ function extractCourseStartingLetter(selector: string): string {
 }
 
 async function main() {
-  const browser: Browser = await puppeteer.launch({ headless: false });
+  const browser: Browser = await puppeteer.launch();
   const page: Page = await browser.newPage();
 
   await page.goto(URL, { waitUntil: 'networkidle2' });
@@ -23,7 +49,7 @@ async function main() {
   await page.waitForNetworkIdle({ concurrency: 2 });
   const alphaNumLinkArr = await page.$$('a.PSPAGE');
 
-  const listOfSubjects: string[] = [];
+  const listOfSubjects: subject[] = [];
   for (let i = 0; i < alphaNumLinkArr.length; i++) {
     const currElem = (await alphaNumLinkArr[i].remoteObject()) as Protocol.Runtime.RemoteObject;
     const currSelector = currElem.description as string;
@@ -42,7 +68,7 @@ async function main() {
     console.log('Courses found\n');
 
     const result = await page.evaluate(async () => {
-      const results: string[] = [];
+      const results: subject[] = [];
 
       const courseCodeElements = document.querySelectorAll('span.PSEDITBOX_DISPONLY');
 
@@ -51,7 +77,7 @@ async function main() {
         if (currElem !== currElem.toUpperCase() || currElem.length > 4) {
           continue;
         }
-        results.push(courseCodeElements[j].innerHTML);
+        results.push({ subject: courseCodeElements[j].innerHTML });
       }
       return results;
     });
@@ -61,6 +87,8 @@ async function main() {
 
   await page.close();
   await browser.close();
+
+  updateAvailableCourses(listOfSubjects);
 }
 
 main();
