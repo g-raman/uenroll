17d24385265a400d3b0768649dbe0db00699326b
Raman Gupta <gr.gupta.raman@gmail.com>
2024-06-22 09:20:48 -0400

    feat: add types for courses, sessions, course components and course details

--- apps/scraper/src/utils/types.ts
+++ apps/scraper/src/utils/types.ts
@@ -6,3 +6,37 @@ export type term = {
 export type subject = {
   subject: string;
 };
+
+export type Course = {
+  courseCode: string;
+  courseTitle: string;
+  term: string;
+};
+
+export type CourseComponent = {
+  courseCode: string;
+  subSection: string;
+  type: string;
+  isOpen: boolean;
+  section: string;
+  term: string;
+};
+
+export type Session = {
+  courseCode: string;
+  section: string;
+  subSection: string;
+  dayOfWeek: string;
+  startTime: string;
+  endTime: string;
+  startDate: string;
+  endDate: string;
+  instructor: string;
+  term: string;
+};
+
+export type CourseDetails = {
+  courses: Course[];
+  courseComponents: CourseComponent;
+  sessions: Session[];
+};
