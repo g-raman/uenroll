1775bf7b6d891eed359509ecf90f2d725ed675de
Raman Gupta <gr.gupta.raman@gmail.com>
2024-11-08 20:34:24 -0500

    feat: update scraper to use deno

--- apps/scraper/src/scrape.sh
+++ apps/scraper/src/scrape.sh
@@ -13,9 +13,9 @@ do
   CURR_OFFSET=$((i * $LIMIT))
   SUBJECTS=$(curl --silent "${DB_ENDPOINT}?select=subject&order=subject.asc&limit=$LIMIT&offset=$CURR_OFFSET" \
   -H "apikey: $KEY" \
-  -H "Authorization: Bearer $KEY" | jq -c '[.[] | .subject]')
+  -H "Authorization: Bearer $KEY" | jq -r '.[] | .subject')
 
-  pnpm tsx --no-deprecation src/index.ts "$SUBJECTS"
+  deno task scrape:courses $SUBJECTS
 
   curr_end_time=$(date +%s)
   curr_execution_time=$((curr_end_time - start_time))
