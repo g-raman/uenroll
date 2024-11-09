b18dcc6fedcbd62ca39f53764a38dca08a4b4598
Raman Gupta <gr.gupta.raman@gmail.com>
2024-11-08 20:51:42 -0500

    refactor: use browser utils to scrape available terms

--- apps/scraper/src/scrapeAvailableTerms.ts
+++ apps/scraper/src/scrapeAvailableTerms.ts
@@ -1,10 +1,11 @@
-import puppeteer, { Browser, Page } from 'npm:puppeteer';
 import { URL } from './utils/constants.ts';
 import supabase from './supabase.ts';
 import { term } from './utils/types.ts';
+import { getBrowserEndpoint, withBrowser, withPage } from './utils/browser.ts';
 
 async function updateAvailableTerms(availableTerms: term[]): Promise<void> {
   console.log('Inserting new available terms...');
+
   const { error } = await supabase.from('availableTerms').upsert(availableTerms, { onConflict: 'term' }).select();
   if (error) {
     console.error('Error: Something went wrong when inserting new available terms data');
@@ -15,35 +16,37 @@ async function updateAvailableTerms(availableTerms: term[]): Promise<void> {
 }
 
 async function main() {
-  const browser: Browser = await puppeteer.launch({ headless: true });
-  const page: Page = await browser.newPage();
+  const browserEndpoint = await getBrowserEndpoint();
 
-  await page.goto(URL, { waitUntil: 'networkidle2' });
+  withBrowser(browserEndpoint, async (browser) => {
+    await withPage(browser)(async (page) => {
+      await page.goto(URL, { waitUntil: 'networkidle2' });
 
-  console.log('Scraping available terms...');
-  const availableTerms: term[] = await page.evaluate(() => {
-    const terms: HTMLSelectElement = document.getElementById('CLASS_SRCH_WRK2_STRM$35$') as HTMLSelectElement;
+      console.log('Scraping available terms...');
 
-    return Array.from(terms.options)
-      .filter((term) => term.value !== '')
-      .map((term) => {
-        const res = {
-          value: term.value,
-          term: term.innerHTML,
-        };
-        return res;
+      const availableTerms: term[] = await page.evaluate(() => {
+        const terms: HTMLSelectElement = document.getElementById('CLASS_SRCH_WRK2_STRM$35$') as HTMLSelectElement;
+
+        return Array.from(terms.options)
+          .filter((term) => term.value !== '')
+          .map((term) => {
+            const res = {
+              value: term.value,
+              term: term.innerHTML,
+            };
+            return res;
+          });
       });
+
+      if (!availableTerms) {
+        console.error('Error: Something went wrong when scraping the available terms');
+        return;
+      }
+      console.log('Successfully scraped available terms');
+
+      updateAvailableTerms(availableTerms);
+    });
   });
-  await page.close();
-  await browser.close();
-
-  if (!availableTerms) {
-    console.error('Error: Something went wrong when scraping the available terms');
-    return;
-  }
-  console.log('Successfully scraped available terms');
-
-  updateAvailableTerms(availableTerms);
 }
 
 main();
