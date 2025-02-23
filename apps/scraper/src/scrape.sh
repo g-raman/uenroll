b3aca25e47fceab25cec7fed8675a3e1d805095e
Raman Gupta <gr.gupta.raman@gmail.com>
2025-02-22 21:59:28 -0500

    fix: dont run commans in background as docker cleanup step needs to wait for everything

--- apps/scraper/src/scrape.sh
+++ apps/scraper/src/scrape.sh
@@ -4,10 +4,9 @@
 docker container run -d -p 9222:9222 zenika/alpine-chrome --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 https://www.chromestatus.com/
 
 start_time=$(date +%s)
-
-nohup deno task scrape:terms &>/var/logs/scraper/scraper.log &
-nohup deno task scrape:subjects &>/var/logs/scraper/scraper.log &
-nohup deno task scrape:courses &>/var/logs/scraper/scraper.log &
+deno task scrape:terms 
+deno task scrape:subjects 
+deno task scrape:courses 
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
