ef336106de17967ff9d440f27569d63e9f2a4753
Raman Gupta <gr.gupta.raman@gmail.com>
2025-02-22 20:48:36 -0500

    feat: add basic script to scrape courses

---
+++ apps/scraper/src/scrape.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Start 
+docker container run -d -p 9222:9222 zenika/alpine-chrome --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 https://www.chromestatus.com/
+
+start_time=$(date +%s)
+
+deno task scrape:terms
+deno task scrape:subjects
+deno task scrape:courses
+
+end_time=$(date +%s)
+execution_time=$((end_time - start_time))
+echo "Total Scraping Time: $execution_time seconds"
+
+# Shutdown docker
+docker stop $(docker ps -q)
+docker container prune -f
