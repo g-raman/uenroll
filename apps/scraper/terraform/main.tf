6f672b16564c741b2f168f2322e485c8017444d1
Raman Gupta <gr.gupta.raman@gmail.com>
2025-02-24 20:40:42 -0500

    feat: use s3 and dynamodb as backend for terraform

--- apps/scraper/terraform/main.tf
+++ apps/scraper/terraform/main.tf
@@ -1,4 +1,13 @@
 terraform {
+  backend "s3" {
+    bucket                  = "uenroll-scraper"
+    dynamodb_table          = "uenroll-scraper-terraform-state-lock"
+    key                     = "scraper"
+    region                  = "us-east-1"
+    profile                 = "scraper"
+    shared_credentials_file = "~/.aws/credentials"
+  }
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
