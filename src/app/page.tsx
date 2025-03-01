ec2fd75a2ebe5633343d1f8d9d1a035f035e463b
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-01 14:36:09 -0500

    fix: wrap provider in suspense block

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,24 +8,27 @@ import Sidebar from "@/layouts/Sidebar/Sidebar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SearchResultsProvider } from "@/contexts/SearchResultsContext";
 import SearchResults from "@/components/SearchResults/SearchResults";
+import { Suspense } from "react";
 
 export default function Page() {
   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <SearchResultsProvider>
-        <App>
-          <Main>
-            <NewCalendar />
-          </Main>
+    <Suspense fallback={<div>Loading...</div>}>
+      <QueryClientProvider client={queryClient}>
+        <SearchResultsProvider>
+          <App>
+            <Main>
+              <NewCalendar />
+            </Main>
 
-          <Sidebar>
-            <SearchBar />
-            <SearchResults />
-          </Sidebar>
-        </App>
-      </SearchResultsProvider>
-    </QueryClientProvider>
+            <Sidebar>
+              <SearchBar />
+              <SearchResults />
+            </Sidebar>
+          </App>
+        </SearchResultsProvider>
+      </QueryClientProvider>
+    </Suspense>
   );
 }
