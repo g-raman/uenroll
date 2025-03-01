16d45566f3ca5d21d5d69b3e14592c7fb527375d
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-01 05:53:56 -0500

    feat: store state object in url

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
+import { NuqsAdapter } from "nuqs/adapters/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,14 +19,16 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Toaster
-          position="top-right"
-          toastOptions={{
-            duration: 3000,
-            className: "w-[90%]",
-          }}
-        />
-        {children}
+        <NuqsAdapter>
+          <Toaster
+            position="top-right"
+            toastOptions={{
+              duration: 3000,
+              className: "w-[90%]",
+            }}
+          />
+          {children}
+        </NuqsAdapter>
       </body>
     </html>
   );
