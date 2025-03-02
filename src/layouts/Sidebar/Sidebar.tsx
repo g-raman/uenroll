6e6921f67e91444679acb25b4c96f88806485b3a
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-02 15:04:12 -0500

    feat: add header

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -1,10 +1,12 @@
 import { Footer } from "../Footer/Footer";
+import Header from "../Header/Header";
 
 export default function Sidebar({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="flex flex-col justify-between overflow-y-scroll h-[40%] bg-white w-full rounded-t-md md:h-full md:w-1/4 md:rounded-md">
+      <Header />
       {children}
       <Footer />
     </div>
