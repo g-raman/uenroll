284162f08028d81b27044d06c7083217eff1d211
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-02 14:12:33 -0500

    feat: add footer

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -1,9 +1,14 @@
+import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Footer } from "../Footer/Footer";
+
 export default function Sidebar({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="overflow-y-scroll h-[40%] bg-white w-full rounded-t-md md:h-full md:w-1/4 md:rounded-md">
+    <div className="flex flex-col justify-between overflow-y-scroll h-[40%] bg-white w-full rounded-t-md md:h-full md:w-1/4 md:rounded-md">
       {children}
+      <Footer />
     </div>
   );
 }
