7e9f13ca54c7349ed0c561e17c68125acfb43ba4
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-02 20:27:04 -0500

    feat: put search section on top for mobile

--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ export default function Sidebar({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex flex-col justify-between overflow-y-scroll h-[40%] bg-white w-full rounded-t-md md:h-full md:w-1/4 md:rounded-md">
+    <div className="flex flex-col order-first md:order-last md:justify-between overflow-y-scroll h-[48%] bg-white w-full rounded-t-md md:h-full md:w-1/4 md:rounded-md">
       <Header />
       {children}
       <Footer />
