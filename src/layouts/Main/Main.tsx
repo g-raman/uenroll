6868aab4428aa99e1319bd2c16071317798b28e1
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-03 10:32:59 -0500

    feat: adjust sizing of search area and calendar for mobile

--- src/layouts/Main/Main.tsx
+++ src/layouts/Main/Main.tsx
@@ -2,7 +2,7 @@ export default function Main({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="p-2 h-[50%] rounded-t-md bg-white md:h-full md:w-3/4 md:rounded-md">
+    <div className="p-2 h-[46%] rounded-t-md bg-white md:h-full md:w-3/4 md:rounded-md">
       {children}
     </div>
   );
--- src/layouts/Sidebar/Sidebar.tsx
+++ src/layouts/Sidebar/Sidebar.tsx
@@ -5,7 +5,7 @@ export default function Sidebar({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex flex-col order-first md:order-last md:justify-between overflow-y-scroll h-[49%] rounded-b-md bg-white w-full md:h-full md:w-1/4 md:rounded-md">
+    <div className="flex flex-col order-first md:order-last md:justify-between overflow-y-scroll h-[53%] rounded-b-md bg-white w-full md:h-full md:w-1/4 md:rounded-md">
       <Header />
       {children}
       <Footer />
