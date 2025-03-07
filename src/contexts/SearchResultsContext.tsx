f4a051647c42721848353aed21b16e3b6570f582
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-06 23:01:58 -0500

    feat: make separate file for helper functions

--- src/contexts/SearchResultsContext.tsx
+++ src/contexts/SearchResultsContext.tsx
@@ -18,6 +18,7 @@ import React, {
 } from "react";
 import LZString from "lz-string";
 import { dayOfWeekToNumberMap, INITIAL_COLOURS } from "@/utils/constants";
+import { createSession, shuffleArray } from "@/utils/helpers";
 
 interface SearchResultsContextType {
   courses: Course[];
@@ -41,36 +42,6 @@ const SearchResultsContext = createContext<
 >(undefined);
 
 const shuffledColours = shuffleArray(INITIAL_COLOURS);
-
-function shuffleArray(array: string[]) {
-  for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
-  }
-  return array;
-}
-
-const createSession = (
-  session: Session,
-  component: Component,
-  course: Course,
-) => ({
-  startTime: session.startTime.slice(0, -3),
-  endTime: session.endTime.slice(0, -3),
-  startRecur: session.startDate,
-  endRecur: session.endDate,
-  dayOfWeek: dayOfWeekToNumberMap[session.dayOfWeek] as number,
-  courseDetails: {
-    backgroundColour: course.colour as string,
-    courseCode: course.courseCode,
-    courseTitle: course.courseTitle,
-    term: course.term,
-    subSection: component.subSection,
-    instructor: session.instructor,
-    type: component.type,
-    isOpen: component.isOpen,
-  },
-});
 
 export const SearchResultsProvider: React.FC<{ children: ReactNode }> = ({
   children,
