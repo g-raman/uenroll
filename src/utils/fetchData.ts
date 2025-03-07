9f87d71fcfe4eb665c6781ffe38dc79d6d559c9a
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-07 12:00:33 -0500

    refactor: move fetch function for courses to separate file

---
+++ src/utils/fetchData.ts
@@ -0,0 +1,26 @@
+import { Term } from "@/types/Types";
+
+export const fetchCourses = async (courseCode: string, term: Term | null) => {
+  if (!term) {
+    throw new Error("No Term Selected");
+  }
+
+  if (courseCode.length < 7) {
+    throw new Error("Not a valid course code");
+  }
+
+  const containsNumber = (str: string): boolean => /\d/.test(str);
+  const containsLetters = (str: string): boolean => /[a-zA-Z]/.test(str);
+
+  if (!containsNumber(courseCode) || !containsLetters(courseCode)) {
+    throw new Error("Not a valid course code");
+  }
+
+  const res = await fetch(`/api/v1/terms/${term.value}/courses/${courseCode}`);
+
+  const data = await res.json();
+  if (data.error) {
+    throw new Error(data.error);
+  }
+  return data.data;
+};
