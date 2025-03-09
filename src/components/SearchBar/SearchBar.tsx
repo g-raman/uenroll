2cfbb3abaab97e7042e3268e61455087d287054e
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-09 15:25:13 -0400

    cleanup: remove initial url parsing from search bar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -34,52 +34,6 @@ export default function SearchBar() {
    * another result
    */
 
-  const {
-    isLoading: isLoadingInitial,
-    error: errorInitial,
-    data: dataInitial,
-  } = useQuery<Course[]>({
-    queryKey: ["coursesInitial", state.term, state.selected],
-    queryFn: async () => {
-      if (!state.term || !state.selected) {
-        return [];
-      }
-      const toFetch = Object.keys(state.selected).map((courseCode) =>
-        fetchCourses(courseCode, state.term),
-      );
-      try {
-        const result = await Promise.all(toFetch);
-        return result;
-      } catch (error) {
-        throw new Error(error as string);
-      }
-      return [];
-    },
-    retry: false,
-  });
-
-  useEffect(() => {
-    if (errorInitial) dispatch({ type: "reset_selected" });
-
-    if (!dataInitial) return;
-
-    /*
-     * Using a setTimeout here to avoid a race condition
-     * when adding new courses.
-     * The colour for each course is assigned in a useEffect on mount/unmount
-     * and they all mutate the same variable.
-     * Simple but hacky fix to just add a delay between each course.
-     */
-    const processCourses = async (courses: Course[]) => {
-      for (const course of courses) {
-        await new Promise((resolve) => setTimeout(resolve, 100));
-        dispatch({ type: "add_course", payload: course });
-      }
-    };
-
-    processCourses(dataInitial);
-  }, [dataInitial, dispatch, errorInitial]);
-
   useEffect(() => {
     if (isSuccess) {
       dispatch({ type: "add_course", payload: data });
@@ -117,15 +71,15 @@ export default function SearchBar() {
           className="border-slate-400 bg-slate-100 border text-sm w-full px-4 py-2 rounded-sm disabled:bg-slate-300"
           type="text"
           placeholder="Course Code Eg. CSI 2101"
-          disabled={isLoading || isLoadingInitial}
+          disabled={isLoading}
         />
 
         <button
           onClick={handleSearchClick}
           className="w-min h-full px-4 bg-[#8f001b] rounded-sm text-white disabled:bg-opacity-40"
-          disabled={isLoading || isLoadingInitial}
+          disabled={isLoading}
         >
-          {isLoading || isLoadingInitial ? (
+          {isLoading ? (
             <FontAwesomeIcon className="size-4 animate-spin" icon={faSpinner} />
           ) : (
             <FontAwesomeIcon className="size-4" icon={faMagnifyingGlass} />
