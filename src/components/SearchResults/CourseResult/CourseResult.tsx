240d5ee29fe79bfcdefe9d6b6cf9d1ac93b9e0a6
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-09 19:58:23 -0400

    refactor: add handler methods instead of using inline functions

--- src/components/SearchResults/CourseResult/CourseResult.tsx
+++ src/components/SearchResults/CourseResult/CourseResult.tsx
@@ -13,11 +13,19 @@ const CourseResult: React.FC<CourseResultProps> = ({ course }) => {
   const [isOpen, setIsOpen] = useState(false);
   const { dispatch } = useSearchResults();
 
+  const handleCourseToggle = () => {
+    setIsOpen((previous) => !previous);
+  };
+
+  const handleCourseRemoval = () => {
+    dispatch({ type: "remove_course", payload: course });
+  };
+
   return (
     <div className="pb-4 text-sm">
       <div className="border rounded-md overflow-hidden">
         <div
-          onClick={() => setIsOpen((is) => !is)}
+          onClick={handleCourseToggle}
           className={`hover:cursor-pointer p-2 ${course.colour}`}
         >
           <div className="flex items-center justify-between">
@@ -26,12 +34,7 @@ const CourseResult: React.FC<CourseResultProps> = ({ course }) => {
             </div>
 
             <div className="flex gap-6 md:gap-4 ml-4 items-baseline">
-              <FontAwesomeIcon
-                onClick={() =>
-                  dispatch({ type: "remove_course", payload: course })
-                }
-                icon={faTrash}
-              />
+              <FontAwesomeIcon onClick={handleCourseRemoval} icon={faTrash} />
               <FontAwesomeIcon
                 className={`transition-all ease-in delay-100 ${
                   isOpen ? "rotate-0" : "rotate-180"
@@ -41,6 +44,7 @@ const CourseResult: React.FC<CourseResultProps> = ({ course }) => {
             </div>
           </div>
         </div>
+
         {course.sections.map((section) => {
           return (
             <div
