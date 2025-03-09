3953c51d959ebca6f1571b6a3bc8272439c6d08a
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-09 16:03:04 -0400

    feat: remove useQuery hook for fetching available terms

--- src/components/TermSelector/TermSelector.tsx
+++ src/components/TermSelector/TermSelector.tsx
@@ -8,11 +8,7 @@ import React, { ChangeEvent } from "react";
 import toast from "react-hot-toast";
 
 export default function TermSelector() {
-  const { state, dispatch, termsQueryState } = useSearchResults();
-
-  if (termsQueryState.isError && termsQueryState.error) {
-    toast.error(termsQueryState.error.message);
-  }
+  const { state, dispatch } = useSearchResults();
 
   function handleSelect(event: ChangeEvent<HTMLSelectElement>) {
     const term = JSON.parse(event.target.value) as Term;
@@ -21,7 +17,7 @@ export default function TermSelector() {
 
   return (
     <>
-      {termsQueryState.isLoading ? (
+      {state.availableTerms.length === 0 ? (
         <div className="h-8 animate-pulse p-2 bg-slate-200 border-slate-400 border rounded-sm"></div>
       ) : (
         <select
@@ -29,12 +25,11 @@ export default function TermSelector() {
           onChange={handleSelect}
           className="w-full bg-slate-100 border-slate-400 border p-2 rounded-sm text-sm"
         >
-          {termsQueryState.isSuccess &&
-            state.availableTerms.map((elem) => (
-              <option key={elem.value} value={JSON.stringify(elem)}>
-                {elem.term}
-              </option>
-            ))}
+          {state.availableTerms.map((elem) => (
+            <option key={elem.value} value={JSON.stringify(elem)}>
+              {elem.term}
+            </option>
+          ))}
         </select>
       )}
     </>
