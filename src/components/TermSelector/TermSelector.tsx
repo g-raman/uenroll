a35a5f5cc36a36a533e9840f9ebdda7099693dbe
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-09 21:53:02 -0400

    fix: add cursor pointer class for clickable elements

--- src/components/TermSelector/TermSelector.tsx
+++ src/components/TermSelector/TermSelector.tsx
@@ -17,24 +17,21 @@ export default function TermSelector() {
 
   return (
     <>
-      {state.availableTerms.length === 0
-        ? (
-          <div className="h-8 animate-pulse p-2 bg-slate-200 border-slate-400 border rounded-xs">
-          </div>
-        )
-        : (
-          <select
-            value={state.term ? JSON.stringify(state.term) : ""}
-            onChange={handleSelect}
-            className="w-full bg-slate-100 border-slate-400 border p-2 rounded-xs text-sm"
-          >
-            {state.availableTerms.map((elem) => (
-              <option key={elem.value} value={JSON.stringify(elem)}>
-                {elem.term}
-              </option>
-            ))}
-          </select>
-        )}
+      {state.availableTerms.length === 0 ? (
+        <div className="h-8 animate-pulse p-2 bg-slate-200 border-slate-400 border rounded-xs"></div>
+      ) : (
+        <select
+          value={state.term ? JSON.stringify(state.term) : ""}
+          onChange={handleSelect}
+          className="cursor-pointer w-full bg-slate-100 border-slate-400 border p-2 rounded-xs text-sm"
+        >
+          {state.availableTerms.map((elem) => (
+            <option key={elem.value} value={JSON.stringify(elem)}>
+              {elem.term}
+            </option>
+          ))}
+        </select>
+      )}
     </>
   );
 }
