68103fc442864628e0588ecc59a111ffee90c577
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-11 09:11:28 -0400

    feat: add disabled states for rest of buttons

--- src/components/CopyLinkButton/CopyLinkButton.tsx
+++ src/components/CopyLinkButton/CopyLinkButton.tsx
@@ -1,12 +1,16 @@
+import { useSearchResults } from "@/contexts/SearchResultsContext";
 import { faCheck, faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React, { useState } from "react";
 
 export const CopyLinkButton = () => {
   const [isCopied, setIsCopied] = useState(false);
+  const { state } = useSearchResults();
+
   return (
     <button
-      className="cursor-pointer flex gap-1 justify-center items-center w-full border-slate-400 border py-3 px-2 h-full rounded-xs text-black"
+      className="hover:bg-slate-100 active:bg-slate-200 disabled:hover:bg-transparent disabled:cursor-not-allowed disabled:text-gray-300 cursor-pointer flex gap-1 justify-center items-center w-full border-slate-400 border py-3 px-2 h-full rounded-xs text-black"
+      disabled={state.selectedSessions.length === 0}
       onClick={async () => {
         try {
           await navigator.clipboard.writeText(window.location.href);
