00aa280119220b3b72d52eea2a3a3de82e8f4735
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-11 08:49:14 -0400

    feat: add method for generating ics calendar

---
+++ src/utils/generateICS.ts
@@ -0,0 +1,40 @@
+import { SelectedSession } from "@/types/Types";
+import dayjs from "dayjs";
+import { generateIcsCalendar, VEvent } from "ts-ics";
+import { v4 } from "uuid";
+
+const getEvents = (sessions: SelectedSession[]): VEvent[] => {
+  return sessions.map((session) => {
+    const baseStartTime = dayjs(`${session.startRecur} ${session.startTime}`);
+    const baseEndTime = dayjs(`${session.startRecur} ${session.endTime}`);
+    const dayOffset = Math.abs(baseStartTime.get("d") - session.dayOfWeek);
+
+    const startTime = baseStartTime.add(dayOffset, "days");
+    const endTime = baseEndTime.add(dayOffset, "days");
+
+    const recurrenceUntil = dayjs(`${session.endRecur} ${session.endTime}`);
+
+    const event: VEvent = {
+      uid: v4(),
+      stamp: { date: new Date() },
+      start: { date: startTime.toDate() },
+      end: { date: endTime.toDate() },
+      recurrenceRule: {
+        frequency: "WEEKLY",
+        until: { date: recurrenceUntil.toDate() },
+      },
+      summary: `${session.courseDetails.courseCode} ${session.courseDetails.courseTitle} - ${session.courseDetails.type}`,
+    };
+
+    return event;
+  });
+};
+
+export const getCalendar = (sessions: SelectedSession[]) => {
+  const events = getEvents(sessions);
+  return generateIcsCalendar({
+    version: "2.0",
+    prodId: "//uEnroll//Calendar Export 1.0//EN",
+    events,
+  });
+};
