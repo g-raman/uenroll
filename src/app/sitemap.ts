9fafbbcd12176c1d9f17c01909eb01a81b5688c5
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-18 19:58:02 -0400

    feat: use base_url from constants files

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
+import { BASE_URL } from "@/utils/constants";
 import type { MetadataRoute } from "next";
 
-const BASE_URL = "https://uenroll.ca";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
