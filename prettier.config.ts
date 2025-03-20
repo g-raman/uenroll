bac441c86c08ac657ab0b92380e57f8b5fb5a24a
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-20 11:37:55 -0400

    feat: add prettier config

---
+++ prettier.config.ts
@@ -0,0 +1,16 @@
+import type { Config } from "prettier";
+
+const config: Config = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: "all",
+  tabWidth: 2,
+  printWidth: 100,
+  arrowParens: "avoid",
+  bracketSpacing: true,
+  endOfLine: "lf",
+  jsxSingleQuote: false,
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+export default config;
