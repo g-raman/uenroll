e8e40543f17c725d41a3a8684bc3cfc70f5292a6
Raman Gupta <gr.gupta.raman@gmail.com>
2025-03-23 08:31:55 -0400

    feat: remove puppeteer usage from main scraping file

--- apps/scraper/src/main.ts
+++ apps/scraper/src/main.ts
@@ -1,36 +1,25 @@
 import scrapeSearchResults from "./scrape/scrapeSearchResults.ts";
-import searchForCourse from "./scrape/searchForCourse.ts";
 import getAvailableTerms, {
   getAvailableSubjects,
   upsertCourseDetails,
 } from "./supabase.ts";
-import { getBrowser, getBrowserEndpoint } from "./utils/browser.ts";
-import {
-  COURSE_REGISTRY_URL,
-  FIRST_YEAR,
-  LAST_YEAR,
-} from "./utils/constants.ts";
+import { FIRST_YEAR, LAST_YEAR } from "./utils/constants.ts";
 import "jsr:@std/dotenv/load";
 import * as cheerio from "cheerio";
 import { getError } from "./utils/scrape.ts";
+import getCourseHTML from "./scrape/getCourse.ts";
 
 const terms = await getAvailableTerms();
-const browserEndpoint = await getBrowserEndpoint();
 
-const browser = await getBrowser(browserEndpoint);
-const page = await browser.newPage();
-
-const term = terms[0];
+const term = terms[1];
 const courses = await getAvailableSubjects();
 
 for (const course of courses) {
   for (let year = FIRST_YEAR; year < LAST_YEAR; year++) {
     console.log(`Searching for ${course} year ${year} courses:`);
-    await page.goto(COURSE_REGISTRY_URL, { waitUntil: "networkidle0" });
-    await searchForCourse(page, term, course, year);
-    await page.waitForNetworkIdle({ concurrency: 0 });
+    const html = await getCourseHTML(term, year, course);
+    if (html.length === 0) continue;
 
-    const html = await page.content();
     const parser = cheerio.load(html);
 
     const error = getError(parser);
@@ -55,6 +44,4 @@ for (const course of courses) {
   }
 }
 
-await page.close();
-await browser.disconnect();
 Deno.exit(0);
