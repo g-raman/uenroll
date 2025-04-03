28457b43ac6b2c521a6a2b2d2c8d41155b1c4d89
Raman Gupta <gr.gupta.raman@gmail.com>
2025-04-03 09:59:18 -0400

    feat: scrape courses for all available terms

--- apps/scraper/src/main.ts
+++ apps/scraper/src/main.ts
@@ -11,36 +11,37 @@ import getSubjectByYear from "./scrape/getSubjectByYear.ts";
 
 const terms = await getAvailableTerms();
 
-const term = terms[1];
 const courses = await getAvailableSubjects();
 
-for (const course of courses) {
-  for (let year = FIRST_YEAR; year < LAST_YEAR; year++) {
-    console.log(`Searching for ${course} year ${year} courses:`);
-    const html = await getSubjectByYear(term, year, course);
-    if (html.length === 0) continue;
+for (const term of terms) {
+  for (const course of courses) {
+    for (let year = FIRST_YEAR; year < LAST_YEAR; year++) {
+      console.log(`Searching for ${course} year ${year} courses:`);
+      const html = await getSubjectByYear(term, year, course);
+      if (html.length === 0) continue;
 
-    const parser = cheerio.load(html);
+      const parser = cheerio.load(html);
 
-    const error = getError(parser);
-    if (error != null) {
-      console.log(error + "\n");
-      continue;
+      const error = getError(parser);
+      if (error != null) {
+        console.log(error + "\n");
+        continue;
+      }
+
+      const results = scrapeSearchResults(parser, term);
+      console.log("Results scraped.");
+      if (
+        results.courses.length === 0 ||
+        results.courseComponents.length === 0 ||
+        results.sessions.length === 0
+      ) {
+        console.log("No results found.");
+      } else {
+        await upsertCourseDetails(results);
+      }
+
+      console.log(`Year ${year} processing complete.\n`);
     }
-
-    const results = scrapeSearchResults(parser, term);
-    console.log("Results scraped.");
-    if (
-      results.courses.length === 0 ||
-      results.courseComponents.length === 0 ||
-      results.sessions.length === 0
-    ) {
-      console.log("No results found.");
-    } else {
-      await upsertCourseDetails(results);
-    }
-
-    console.log(`Year ${year} processing complete.\n`);
   }
 }
 
