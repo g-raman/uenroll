6c6db5dbf9850979356726bec42ca51a4d025b6a
Raman Gupta <gr.gupta.raman@gmail.com>
2025-04-03 10:12:32 -0400

    feat: adjust script to only use docker for getting all subjects

--- apps/scraper/src/scrape.sh
+++ apps/scraper/src/scrape.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# Start 
+# Still need to use Puppeteer for Subjects
 docker container run -d -p 9222:9222 zenika/alpine-chrome --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 https://www.chromestatus.com/
+deno task scrape:subjects 
+docker stop $(docker ps -q)
+docker container prune -f
 
 start_time=$(date +%s)
 deno task scrape:terms 
-deno task scrape:subjects 
 deno task scrape:courses 
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
 echo "Total Scraping Time: $execution_time seconds"
 
-# Shutdown docker
-docker stop $(docker ps -q)
-docker container prune -f
