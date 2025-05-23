ec6cbb4f05d92c2e9de090bbba3be3dc80f5beb5
Raman Gupta <gr.gupta.raman@gmail.com>
2025-05-23 12:34:17 -0400

    feat: use db package in cleanup file

--- apps/scraper/src/cleanup.ts
+++ apps/scraper/src/cleanup.ts
@@ -1,8 +1,5 @@
-import {
-  client,
-  removeCoursesWithNoSessions,
-  removeOldSessions,
-} from "../supabase.js";
+import { removeCoursesWithNoSessions, removeOldSessions } from "./supabase.js";
+import { client } from "@repo/db";
 
 const REMOVE_BEFORE_TIME = 5 * 60 * 60 * 1000;
 await removeOldSessions(REMOVE_BEFORE_TIME);
