c177ced05a552a024999b6883557eec83b44dd76
Raman Gupta <gr.gupta.raman@gmail.com>
2025-05-28 10:59:49 -0400

    fix (scraper): update file paths for scrape scripts (#55)
    * fix (scraper): update file paths for scrape scripts
    
    * feat (db): add build script
    
    * feat (db): export transpiled db package files
    
    * refactor (scraper): do build step in ec2 instance setup file
    
    * chore: add changeset

---
+++ .changeset/salty-readers-appear.md
@@ -0,0 +1,6 @@
+---
+"scraper": patch
+"@repo/db": patch
+---
+
+Add build step for DB package and use transpiled files. Build scraper during EC2 instance setup.
--- apps/scraper/src/scrape.sh
+++ apps/scraper/src/scrape.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Build
-pnpm run build
-
 # Still need to use Puppeteer for Subjects
 docker container run -d -p 9222:9222 zenika/alpine-chrome --no-sandbox --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 https://www.chromestatus.com/
 pnpm scrape:subjects 
