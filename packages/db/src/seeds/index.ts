7ef0862aa979c6efe7fa449c1e44d5432ff992d4
Raman Gupta <gr.gupta.raman@gmail.com>
2025-05-30 11:19:43 -0400

    feat (db): add cleanup before seeding

--- packages/db/src/seeds/index.ts
+++ packages/db/src/seeds/index.ts
@@ -1,11 +1,23 @@
-import { client, db } from "../index.js";
+import { client, db, type Database } from "../index.js";
 import { seedAvailableSubjects } from "./availableSubjects.js";
 import { seedAvailableTerms } from "./availableTerms.js";
 import { seedCourseComponents } from "./courseComponents.js";
 import { seedCourses } from "./courses.js";
 import { seedSessions } from "./sessions.js";
+import * as schema from "../schema.js";
+
+async function clean(db: Database) {
+  await db.delete(schema.sessionsTable);
+  await db.delete(schema.courseComponentsTable);
+  await db.delete(schema.coursesTable);
+  await db.delete(schema.availableTermsTable);
+  await db.delete(schema.availableSubjectsTable);
+
+  console.log("Cleared existing data.");
+}
 
 async function main() {
+  await clean(db);
   await seedAvailableSubjects(db);
   await seedAvailableTerms(db);
   await seedCourses(db);
