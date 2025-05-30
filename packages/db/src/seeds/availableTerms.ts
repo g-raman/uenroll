2a1eb25e53b7b8bf7c402e751e594c90674df504
Raman Gupta <gr.gupta.raman@gmail.com>
2025-05-30 10:49:16 -0400

    feat (db): add seed file for available terms

---
+++ packages/db/src/seeds/availableTerms.ts
@@ -0,0 +1,22 @@
+import type { PostgresJsDatabase } from "drizzle-orm/postgres-js";
+import { availableTermsTable } from "../schema.js";
+
+const availableTerms = [
+  {
+    term: "2025 Fall Term",
+    value: "2259",
+  },
+  {
+    term: "2025 Spring/Summer Term",
+    value: "2255",
+  },
+  {
+    term: "2026 Winter Term",
+    value: "2261",
+  },
+];
+
+export async function seedAvailableTerms(db: PostgresJsDatabase) {
+  await db.insert(availableTermsTable).values(availableTerms);
+  console.log("Seeded database with available terms");
+}
