7f30fae78ea805ca5a0a7c77cca10526251177a0
Raman Gupta <gr.gupta.raman@gmail.com>
2025-05-30 11:52:48 -0400

    fix (web): wrap react query client in use state

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -7,14 +7,14 @@ import Sidebar from "@/layouts/Sidebar/Sidebar";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { SearchResultsProvider } from "@/contexts/SearchResultsContext";
 import SearchResults from "@/components/SearchResults/SearchResults";
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import { CopyLinkButton } from "@/components/CopyLinkButton/CopyLinkButton";
 import { DeleteSearchResultsButton } from "@/components/DeleteSearchResultsButton/DeleteSearchResultsButton";
 import Calendar from "@/components/Calendar/Calendar";
 import DownloadCalendarButton from "@/components/DownloadCalendarButton/DownloadCalendarButton";
 
 export default function Page() {
-  const queryClient = new QueryClient();
+  const queryClient = useState(new QueryClient())[0];
 
   return (
     <Suspense fallback={<div>Loading...</div>}>
