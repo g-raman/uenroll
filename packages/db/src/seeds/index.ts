3fdedec623daee3f5a37b7e732698432170a7ca7
Raman Gupta <gr.gupta.raman@gmail.com>
2025-05-30 11:12:17 -0400

    feat (db): add seed script

---
+++ packages/db/src/seeds/index.ts
@@ -0,0 +1,16 @@
+import { db } from "../index.js";
+import { seedAvailableSubjects } from "./availableSubjects.js";
+import { seedAvailableTerms } from "./availableTerms.js";
+import { seedCourseComponents } from "./courseComponents.js";
+import { seedCourses } from "./courses.js";
+import { seedSessions } from "./sessions.js";
+
+async function main() {
+  await seedAvailableSubjects(db);
+  await seedAvailableTerms(db);
+  await seedCourses(db);
+  await seedCourseComponents(db);
+  await seedSessions(db);
+}
+
+main();
