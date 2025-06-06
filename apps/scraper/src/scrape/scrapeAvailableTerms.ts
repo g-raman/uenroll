b88c30548e873f9e18cd43286f333300e1eda8bb
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-06 09:27:51 -0400

    feat (scraper): cleanup outdated terms after scraping new ones

--- apps/scraper/src/scrape/scrapeAvailableTerms.ts
+++ apps/scraper/src/scrape/scrapeAvailableTerms.ts
@@ -3,13 +3,15 @@ import { COURSE_REGISTRY_URL } from "../utils/constants.js";
 import type { Term } from "../utils/types.js";
 import { updateAvailableTerms } from "../supabase.js";
 import { fetchCookie } from "../utils/cookies.js";
-import { client } from "@repo/db";
+import { client, db } from "@repo/db";
+import { availableTermsTable } from "@repo/db/schema";
+import { eq } from "drizzle-orm";
 
 const response = await fetchCookie(COURSE_REGISTRY_URL);
 const html = await response.text();
 const $ = cheerio.load(html);
 
-const terms: Term[] = [];
+const newlyAvailableTerms: Term[] = [];
 $("[id='CLASS_SRCH_WRK2_STRM$35$']")
   .find("option")
   .each(function (this) {
@@ -21,11 +23,39 @@ $("[id='CLASS_SRCH_WRK2_STRM$35$']")
     const term = option.text();
     const value = option.attr("value") as string;
 
-    terms.push({
+    newlyAvailableTerms.push({
       term,
       value,
     });
   });
 
-await updateAvailableTerms(terms);
+const currentAvailableTerms = await db.select().from(availableTermsTable);
+await updateAvailableTerms(newlyAvailableTerms);
+
+const termsToDelete = currentAvailableTerms.filter(
+  currentAvailableTerm =>
+    !newlyAvailableTerms.some(
+      newlyAvailableTerm =>
+        newlyAvailableTerm.term === currentAvailableTerm.term,
+    ),
+);
+const termsToDeletePromise = termsToDelete.map(termToDelete =>
+  db
+    .delete(availableTermsTable)
+    .where(eq(availableTermsTable.term, termToDelete.term)),
+);
+
+const deletionResults = await Promise.allSettled(termsToDeletePromise);
+deletionResults.forEach((result, index) => {
+  const originalTerm = termsToDelete[index] as Term;
+
+  if (result.status === "fulfilled") {
+    console.log(`Successfully deleted outdated term ${originalTerm.term}`);
+  } else {
+    console.error(
+      `Something went wrong when trying to delete outdated term ${originalTerm.term}: ${result.reason.message}`,
+    );
+  }
+});
+
 await client.end();
