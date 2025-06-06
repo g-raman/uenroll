899233c3a0caf2c48b1038e7ca13794766176e6e
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-06 11:46:40 -0400

    feat (web): use autocomplete component

--- apps/web/src/components/SearchBar/SearchBar.tsx
+++ apps/web/src/components/SearchBar/SearchBar.tsx
@@ -1,13 +1,7 @@
 import { useSearchResults } from "@/contexts/SearchResultsContext";
-import { Course, CourseAutocomplete } from "@/types/Types";
+import { Course } from "@/types/Types";
 import { useQuery } from "@tanstack/react-query";
-import React, {
-  ChangeEvent,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from "react";
+import React, { useCallback, useState } from "react";
 import TermSelector from "../TermSelector/TermSelector";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -17,22 +11,17 @@ import {
 import toast from "react-hot-toast";
 import { MAX_RESULTS_ALLOWED } from "@/utils/constants";
 import { fetchAllCourses, fetchCourse } from "@/utils/fetchData";
-import MiniSearch, { SearchResult } from "minisearch";
 import { Button } from "@repo/ui/components/button";
-import { Input } from "@repo/ui/components/input";
+import { AutoComplete } from "@repo/ui/components/autocomplete";
 
 export default function SearchBar() {
   const [query, setQuery] = useState("");
-  const [autoCompleteResults, setAutoCompleteResults] = useState<
-    SearchResult[]
-  >([]);
-  const [isAutoCompleteLoading, setIsAutoCompleteLoading] = useState(false);
-  const [isFocused, setIsFocused] = useState(false);
   const { state, dispatch } = useSearchResults();
+  const [selectedValue, setSelectedValue] = useState("");
 
   const { isLoading, refetch } = useQuery<Course>({
-    queryKey: ["courses", query, state.term],
-    queryFn: () => fetchCourse(query, state.term),
+    queryKey: ["courses", selectedValue, state.term],
+    queryFn: () => fetchCourse(selectedValue, state.term),
     enabled: false,
     retry: false,
     gcTime: 0,
@@ -54,46 +43,6 @@ export default function SearchBar() {
     staleTime: Infinity,
   });
 
-  const search = useMemo(() => {
-    const miniSearch = new MiniSearch<CourseAutocomplete>({
-      fields: ["courseCode", "courseTitle"],
-    });
-
-    if (!dataAllCourses) return miniSearch;
-
-    const parsedCourses = dataAllCourses.map((course, i: number) => {
-      return {
-        ...course,
-        id: i,
-      };
-    });
-
-    miniSearch.addAll(parsedCourses);
-    return miniSearch;
-  }, [dataAllCourses]);
-
-  useEffect(() => {
-    if (!query || !search) {
-      setAutoCompleteResults([]);
-      return;
-    }
-
-    setIsAutoCompleteLoading(true);
-    const timeoutId = setTimeout(() => {
-      const results = search.search(query, {
-        boost: { courseCode: 2 },
-        fuzzy: 0.3,
-        prefix: true,
-      });
-      const topResults = results ? results.slice(0, 5) : [];
-
-      setAutoCompleteResults(topResults);
-      setIsAutoCompleteLoading(false);
-    }, 300);
-
-    return () => clearTimeout(timeoutId);
-  }, [query, search, dataAllCourses]);
-
   const handleSearchClick = useCallback(async () => {
     if (query.length === 0) return;
 
@@ -106,55 +55,32 @@ export default function SearchBar() {
     if (isSuccess) {
       dispatch({ type: "add_course", payload: data });
       setQuery("");
-      setIsFocused(false);
     } else if (error) {
       toast.error(error.message);
     }
   }, [dispatch, query.length, refetch, state.courses.length]);
 
-  const handleFocus = () => {
-    setIsFocused(true);
-  };
-
-  /* Add small delay as clicking an autocomplete item
-   * causes this to trigger to fast and completion to not work
-   * */
-  const handleBlur = () => {
-    setTimeout(() => setIsFocused(false), 100);
-  };
-
-  const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === "Enter") {
-      handleSearchClick();
-    }
-  };
-
-  const handleInputChange = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setQuery(e.target.value);
-  }, []);
-
-  const handleSelectAutoComplete = async (
-    selectedCourse: CourseAutocomplete,
-  ) => {
-    handleBlur();
-    setAutoCompleteResults([]);
-    setQuery(selectedCourse.courseCode);
-  };
-
   return (
     <div className="sticky top-0 z-10 mb-2 mt-4 flex flex-col gap-2 bg-white">
       <TermSelector />
       <div className="flex items-center justify-between gap-2">
-        <Input
-          className="text-sm"
-          value={query}
-          onKeyDown={handleKeyDown}
-          onChange={handleInputChange}
-          type="text"
-          placeholder="Course Code Eg. CSI 2101"
-          disabled={isLoading}
-          onFocus={handleFocus}
-          onBlur={handleBlur}
+        <AutoComplete
+          searchValue={query}
+          selectedValue={selectedValue}
+          onSelectedValueChange={setSelectedValue}
+          onSearchValueChange={setQuery}
+          placeholder="Course Code or Course Name..."
+          items={
+            dataAllCourses
+              ? dataAllCourses.map((course, index) => {
+                  return {
+                    label: `${course.courseCode} ${course.courseTitle}`,
+                    value: course.courseCode,
+                    id: `${index}`,
+                  };
+                })
+              : []
+          }
         />
 
         <Button
@@ -170,47 +96,6 @@ export default function SearchBar() {
           )}
         </Button>
       </div>
-
-      {autoCompleteResults.length === 0 && isFocused ? (
-        <div className="max-h-70 absolute top-24 z-10 w-full overflow-y-auto rounded-sm border border-gray-300 bg-white p-4 text-center text-sm text-gray-500 shadow-lg">
-          {query.length === 0 ? (
-            "Search for a course..."
-          ) : isAutoCompleteLoading ? (
-            <FontAwesomeIcon
-              size="xl"
-              className="animate-spin"
-              icon={faSpinner}
-            />
-          ) : (
-            "No Results Found..."
-          )}
-        </div>
-      ) : (
-        dataAllCourses &&
-        isFocused && (
-          <ul className="max-h-70 absolute top-24 z-20 w-full overflow-y-auto rounded-sm border border-gray-300 bg-white shadow-lg">
-            {autoCompleteResults.map(result => (
-              <li
-                key={result.id}
-                onPointerDown={() =>
-                  handleSelectAutoComplete(
-                    dataAllCourses[result.id] as CourseAutocomplete,
-                  )
-                }
-                className="cursor-pointer px-4 py-2 hover:bg-gray-200"
-              >
-                <div className="text-sm text-gray-800">
-                  {dataAllCourses[result.id]?.courseCode}:&nbsp;
-                  {dataAllCourses[result.id]?.courseTitle.replaceAll(
-                    /\(\+\d+ combined\)/g,
-                    "",
-                  )}
-                </div>
-              </li>
-            ))}
-          </ul>
-        )
-      )}
     </div>
   );
 }
