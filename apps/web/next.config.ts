deab4b76888f5f0423d932c0cbf2e422d585d4ff
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-05 20:28:14 -0400

    feat (web): transpile ui package

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  transpilePackages: ["@repo/db", "@repo/env"],
+  transpilePackages: ["@repo/db", "@repo/env", "@repo/ui"],
   typescript: {
     ignoreBuildErrors: true,
   },
