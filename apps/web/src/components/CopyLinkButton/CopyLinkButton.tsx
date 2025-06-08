8b67ee827eefd13ebfdd54792405f0887990ea3c
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-08 16:10:00 -0400

    feat (web): add tooltip for copy link button

--- apps/web/src/components/CopyLinkButton/CopyLinkButton.tsx
+++ apps/web/src/components/CopyLinkButton/CopyLinkButton.tsx
@@ -2,6 +2,11 @@ import { useSearchResults } from "@/contexts/SearchResultsContext";
 import { faCheck, faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "@repo/ui/components/button";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+} from "@repo/ui/components/tooltip";
 import React, { useState } from "react";
 
 export const CopyLinkButton = () => {
@@ -9,23 +14,33 @@ export const CopyLinkButton = () => {
   const { state } = useSearchResults();
 
   return (
-    <Button
-      className="flex-1"
-      variant="outline"
-      size="lg"
-      disabled={state.selectedSessions.length === 0}
-      onClick={async () => {
-        try {
-          await navigator.clipboard.writeText(window.location.href);
-          setIsCopied(true);
-          setTimeout(() => setIsCopied(false), 2000);
-        } catch (error) {
-          console.log("Failed to copy url: ", error);
-        }
-      }}
-    >
-      <FontAwesomeIcon className="size-4" icon={isCopied ? faCheck : faLink} />
-      <p className="text-xs">{isCopied ? "Copied" : "Copy Link"}</p>
-    </Button>
+    <Tooltip>
+      <TooltipTrigger asChild>
+        <Button
+          className="flex-1"
+          variant="outline"
+          size="lg"
+          disabled={state.selectedSessions.length === 0}
+          onClick={async () => {
+            try {
+              await navigator.clipboard.writeText(window.location.href);
+              setIsCopied(true);
+              setTimeout(() => setIsCopied(false), 2000);
+            } catch (error) {
+              console.log("Failed to copy url: ", error);
+            }
+          }}
+        >
+          <FontAwesomeIcon
+            className="size-4"
+            icon={isCopied ? faCheck : faLink}
+          />
+          <p className="text-xs">{isCopied ? "Copied" : "Copy Link"}</p>
+        </Button>
+      </TooltipTrigger>
+      <TooltipContent>
+        <p>Copy the link for this schedule</p>
+      </TooltipContent>
+    </Tooltip>
   );
 };
