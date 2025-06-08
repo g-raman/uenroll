dc449f4518e48b2d222d96d35bc08439c6b25fd3
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-08 14:53:42 -0400

    refactor (web): change structure of accordion rendering

--- apps/web/src/components/SearchResults/CourseResult/CourseResult.tsx
+++ apps/web/src/components/SearchResults/CourseResult/CourseResult.tsx
@@ -1,19 +1,14 @@
 import { Course } from "@/types/Types";
 import { SectionResult } from "../SectionResult/SectionResult";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faArrowUpRightFromSquare,
-  faTrash,
-} from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 import { useSearchResults } from "@/contexts/SearchResultsContext";
-import { Button } from "@repo/ui/components/button";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@repo/ui/components/accordion";
-import { useState } from "react";
 
 interface CourseResultProps {
   course: Course;
@@ -21,64 +16,43 @@ interface CourseResultProps {
 
 const CourseResult: React.FC<CourseResultProps> = ({ course }) => {
   const { dispatch } = useSearchResults();
-  const [isBottomRounded, setIsBottomRounded] = useState(true);
 
   const handleCourseRemoval = () => {
     dispatch({ type: "remove_course", payload: course });
   };
 
   return (
-    <div className="pb-4 text-sm">
-      <Button variant="link" className="h-min px-2 py-1">
-        <a
-          href={`https://uo.zone/course/${course.courseCode.toLowerCase()}`}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Best Professors for {course.courseCode}&nbsp;
-          <FontAwesomeIcon size={"sm"} icon={faArrowUpRightFromSquare} />
-        </a>
-      </Button>
-
-      <Accordion
-        type="single"
-        collapsible
-        // HACK: Custom logic to round out corners when accordion is collapsed
-        onValueChange={() => setIsBottomRounded(previous => !previous)}
+    <>
+      <AccordionTrigger
+        className={`cursor-pointer items-center p-2 font-normal ${course.colour}`}
       >
-        <AccordionItem value="course-1">
-          <AccordionTrigger
-            className={`cursor-pointer items-center p-2 font-normal ${course.colour} ${isBottomRounded ? "" : "rounded-b-none"}`}
-          >
-            <p className="truncate">{`${course.courseCode}: ${course.courseTitle}`}</p>
+        <p className="truncate">{`${course.courseCode}: ${course.courseTitle}`}</p>
 
-            <FontAwesomeIcon
-              className="ml-auto !rotate-0"
-              onClick={handleCourseRemoval}
-              icon={faTrash}
-            />
-          </AccordionTrigger>
+        <FontAwesomeIcon
+          className="ml-auto !rotate-0"
+          onClick={handleCourseRemoval}
+          icon={faTrash}
+        />
+      </AccordionTrigger>
 
-          <AccordionContent className="p-0">
-            <Accordion
-              type="multiple"
-              className="overflow-hidden rounded-b-sm border-x border-slate-200"
-            >
-              {course.sections.map(section => {
-                return (
-                  <AccordionItem
-                    value={`${course.courseCode}${section.section}`}
-                    key={`${course.courseCode}${section.section}`}
-                  >
-                    <SectionResult section={section} course={course} />
-                  </AccordionItem>
-                );
-              })}
-            </Accordion>
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
-    </div>
+      <AccordionContent className="p-0">
+        <Accordion
+          type="multiple"
+          className="overflow-hidden rounded-b-sm border-x border-slate-200"
+        >
+          {course.sections.map(section => {
+            return (
+              <AccordionItem
+                value={`${course.courseCode}${section.section}`}
+                key={`${course.courseCode}${section.section}`}
+              >
+                <SectionResult section={section} course={course} />
+              </AccordionItem>
+            );
+          })}
+        </Accordion>
+      </AccordionContent>
+    </>
   );
 };
 
--- apps/web/src/components/SearchResults/SearchResults.tsx
+++ apps/web/src/components/SearchResults/SearchResults.tsx
@@ -1,5 +1,9 @@
 import { useSearchResults } from "@/contexts/SearchResultsContext";
 import CourseResult from "./CourseResult/CourseResult";
+import { Button } from "@repo/ui/components/button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Accordion, AccordionItem } from "@repo/ui/components/accordion";
+import { faArrowUpRightFromSquare } from "@fortawesome/free-solid-svg-icons";
 
 export default function SearchResults() {
   const { state } = useSearchResults();
@@ -7,7 +11,26 @@ export default function SearchResults() {
   return (
     <>
       {state.courses.map(course => {
-        return <CourseResult key={`${course.courseCode}`} course={course} />;
+        return (
+          <div className="pb-4 text-sm" key={course.courseCode}>
+            <Button variant="link" className="h-min px-2 py-1">
+              <a
+                href={`https://uo.zone/course/${course.courseCode.toLowerCase()}`}
+                target="_blank"
+                rel="noreferrer"
+              >
+                Best Professors for {course.courseCode}&nbsp;
+                <FontAwesomeIcon size="sm" icon={faArrowUpRightFromSquare} />
+              </a>
+            </Button>
+
+            <Accordion type="multiple">
+              <AccordionItem value={course.courseCode}>
+                <CourseResult course={course} />
+              </AccordionItem>
+            </Accordion>
+          </div>
+        );
       })}
     </>
   );
