5bf819fb65c168b08d6cdbf74be89d578dd9442e
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-09 13:42:41 -0400

    feat (web): hide logo when screen is small

--- apps/web/src/layouts/Header/Header.tsx
+++ apps/web/src/layouts/Header/Header.tsx
@@ -4,7 +4,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export default function Header() {
   return (
-    <div className="bg-background mb-2 flex items-center rounded-b-md p-4 md:mb-4 md:rounded-md">
+    <div className="bg-background mb-2 flex hidden items-center rounded-b-md p-4 sm:flex md:mb-4 md:rounded-md">
       <div className="flex w-full items-baseline justify-start text-4xl">
         <FontAwesomeIcon
           className="text-primary size-12"
