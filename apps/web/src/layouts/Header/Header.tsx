fe1404184f6471c6e4a7871225c11a6744a5d8da
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-09 11:19:28 -0400

    refactor (web): remove hardcoded colour value in header

--- apps/web/src/layouts/Header/Header.tsx
+++ apps/web/src/layouts/Header/Header.tsx
@@ -5,8 +5,7 @@ export default function Header() {
   return (
     <div className="flex items-baseline gap-4 text-5xl font-medium">
       <FontAwesomeIcon
-        className="size-12"
-        color="#8f001b"
+        className="text-primary size-12"
         icon={faBuildingColumns}
       />
       <p>uEnroll</p>
