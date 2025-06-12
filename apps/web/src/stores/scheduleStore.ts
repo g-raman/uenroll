6e7de530be22ee970f4aeb648d7e028a77a884d7
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-12 07:48:35 -0400

    feat (web): add initialize data to store

--- apps/web/src/stores/scheduleStore.ts
+++ apps/web/src/stores/scheduleStore.ts
@@ -1,7 +1,18 @@
 import { Course, Selected, SelectedSession, Term } from "@/types/Types";
 import { INITIAL_COLOURS } from "@/utils/constants";
-import { shuffleArray } from "@/utils/helpers";
+import { createNewSelectedSessions, shuffleArray } from "@/utils/helpers";
 import { create } from "zustand";
+
+export interface InitializeDataPayload {
+  courseSearchResults: Course[];
+  selectedSessionsURL: Selected | null;
+  selectedTerm: Term | null;
+  availableTerms: Term[];
+}
+
+interface ScheduleActions {
+  setInitialData: (initialData: InitializeDataPayload) => void;
+}
 
 interface ScheduleState {
   courseSearchResults: Course[];
@@ -10,6 +21,7 @@ interface ScheduleState {
   colours: string[];
   selectedTerm: Term | null;
   availableTerms: Term[];
+  actions: ScheduleActions;
 }
 
 const useScheduleStore = create<ScheduleState>(set => ({
@@ -19,4 +31,32 @@ const useScheduleStore = create<ScheduleState>(set => ({
   colours: shuffleArray(INITIAL_COLOURS),
   selectedTerm: null,
   availableTerms: [],
+  actions: {
+    setInitialData: initialData =>
+      set(old => {
+        const {
+          selectedTerm,
+          availableTerms,
+          courseSearchResults,
+          selectedSessionsURL,
+        } = initialData;
+        const colours = [...old.colours];
+        courseSearchResults.forEach(course => (course.colour = colours.pop()));
+
+        const selectedSessions = createNewSelectedSessions(
+          courseSearchResults,
+          selectedSessionsURL,
+        );
+
+        return {
+          ...old,
+          selectedTerm,
+          availableTerms,
+          courseSearchResults,
+          selectedSessionsURL,
+          selectedSessions,
+          colours,
+        };
+      }),
+  },
 }));
