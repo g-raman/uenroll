5498dfd64b0785526b982f96b469eae41ab5cae2
Raman Gupta <gr.gupta.raman@gmail.com>
2025-06-29 16:25:24 -0400

    feat (db): add queries for adding scraped information

--- packages/db/src/queries.ts
+++ packages/db/src/queries.ts
@@ -1,9 +1,11 @@
-import { eq, asc } from "drizzle-orm";
+import { eq, asc, sql, lt, notInArray } from "drizzle-orm";
 import { db } from "./index.js";
 import {
   availableSubjectsTable,
   availableTermsTable,
+  courseComponentsTable,
   coursesTable,
+  sessionsTable,
 } from "./schema.js";
 
 export async function getAvailableTerms() {
@@ -35,3 +37,131 @@ export async function getAvailableCoursesByTerm(term: string) {
     .where(eq(coursesTable.term, term))
     .limit(3500);
 }
+
+export const updateCourses = async (
+  courses: typeof coursesTable.$inferInsert,
+) => {
+  try {
+    await db
+      .insert(coursesTable)
+      .values(courses)
+      .onConflictDoUpdate({
+        target: [coursesTable.courseCode, coursesTable.term],
+        set: {
+          courseTitle: sql`excluded.course_title`,
+        },
+      });
+  } catch (error) {
+    throw new Error("Something went wrong when inserting courses:\n" + error);
+  }
+};
+
+export const updateCourseComponents = async (
+  courseComponents: typeof courseComponentsTable.$inferInsert,
+) => {
+  try {
+    await db
+      .insert(courseComponentsTable)
+      .values(courseComponents)
+      .onConflictDoUpdate({
+        target: [
+          courseComponentsTable.courseCode,
+          courseComponentsTable.term,
+          courseComponentsTable.subSection,
+        ],
+        set: {
+          section: sql`excluded.section`,
+          type: sql`excluded.type`,
+        },
+      });
+  } catch (error) {
+    throw new Error(
+      "Something went wrong when inserting course components:\n" + error,
+    );
+  }
+};
+
+export const updateSessions = async (
+  sessions: typeof sessionsTable.$inferInsert,
+) => {
+  try {
+    await db.insert(sessionsTable).values(sessions);
+  } catch (error) {
+    throw new Error("Something went wrong when inserting sessions:\n" + error);
+  }
+};
+
+export const updateAvailableTerms = async (
+  terms: typeof availableTermsTable.$inferInsert,
+): Promise<void> => {
+  try {
+    await db
+      .insert(availableTermsTable)
+      .values(terms)
+      .onConflictDoUpdate({
+        target: availableTermsTable.term,
+        set: {
+          value: sql`excluded.value`,
+        },
+      });
+
+    console.log("Success: Updated available terms");
+  } catch (error) {
+    console.error(
+      "Error: Something went wrong when updating availabe terms: ",
+      error,
+    );
+  }
+};
+
+export const updateAvailableSubjects = async (
+  subjects: typeof availableSubjectsTable.$inferInsert,
+): Promise<void> => {
+  try {
+    await db
+      .insert(availableSubjectsTable)
+      .values(subjects)
+      .onConflictDoNothing();
+
+    console.log("Success: Updated available subjects");
+  } catch (error) {
+    console.error(
+      "Error: Something went wrong when updating availabe subjects: ",
+      error,
+    );
+  }
+};
+
+export const removeOldSessions = async (milliseconds: number) => {
+  try {
+    await db
+      .delete(sessionsTable)
+      .where(
+        lt(sessionsTable.last_updated, new Date(Date.now() - milliseconds)),
+      );
+  } catch (error) {
+    console.error(
+      "Error: Something went wrong when deleting old sessions",
+      error,
+    );
+  }
+};
+
+export const removeCoursesWithNoSessions = async () => {
+  try {
+    const coursesWithSessions = db
+      .selectDistinct({ courseCode: sessionsTable.courseCode })
+      .from(sessionsTable);
+
+    const coursesWithoutSessions = await db
+      .delete(coursesTable)
+      .where(notInArray(coursesTable.courseCode, coursesWithSessions));
+
+    return coursesWithoutSessions;
+  } catch (error) {
+    console.error(
+      "Error: Something went wrong when deleting courses with no sessions",
+      error,
+    );
+  }
+};
