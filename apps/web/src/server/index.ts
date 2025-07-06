7f06ab90c2c02159d52fcf5b5f285106756b498f
Raman Gupta <gr.gupta.raman@gmail.com>
2025-07-06 17:55:12 -0400

    feat (web): add getCourse query

--- apps/web/src/server/index.ts
+++ apps/web/src/server/index.ts
@@ -1,5 +1,30 @@
-import { router } from "./trpc";
+import { getCourse, processCourse } from "@repo/db/queries";
+import { publicProcedure, router } from "./trpc";
+import { z } from "zod";
+import { CourseSearchResult } from "@repo/db/types";
+import { TRPCError } from "@trpc/server";
 
-export const appRouter = router({});
+export const appRouter = router({
+  getCourse: publicProcedure
+    .input(
+      z.object({
+        term: z.string(),
+        courseCode: z.string(),
+      }),
+    )
+    .query(async ({ input }) => {
+      const course = await getCourse(input.term, input.courseCode);
+      const processedCourse = processCourse(course);
+
+      if (processedCourse.isErr()) {
+        throw new TRPCError({
+          code: "NOT_FOUND",
+          message: processedCourse.error.message,
+        });
+      }
+
+      return processedCourse.value;
+    }),
+});
 
 export type AppRouter = typeof appRouter;
