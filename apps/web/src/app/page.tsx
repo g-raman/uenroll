45f68372a1beef6afcfa1405b2b25e4de9af26d7
Raman Gupta <gr.gupta.raman@gmail.com>
2025-07-09 13:36:26 -0400

    cleanup (web): remove all initial fetch logic from page component

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,104 +5,12 @@ import Main from "@/layouts/Main/Main";
 import SearchBar from "@/components/SearchBar/SearchBar";
 import Sidebar from "@/layouts/Sidebar/Sidebar";
 import SearchResults from "@/components/SearchResults/SearchResults";
-import { useEffect, useRef, useState } from "react";
 import { CopyLinkButton } from "@/components/Buttons/CopyLinkButton/CopyLinkButton";
 import { DeleteSearchResultsButton } from "@/components/Buttons/DeleteSearchResultsButton/DeleteSearchResultsButton";
 import Calendar from "@/components/Calendar/Calendar";
 import DownloadCalendarButton from "@/components/Buttons/DownloadCalendarButton/DownloadCalendarButton";
-import {
-  useScheduleActions,
-  useSelectedSessionsURL,
-  useSelectedTerm,
-} from "@/stores/scheduleStore";
-import { useQueryState } from "nuqs";
-import { fetchCourse, fetchTerms } from "@/utils/fetchData";
-import { Term } from "@/types/Types";
-import LZString from "lz-string";
-import { trpc } from "./_trpc/client";
 
 export default function Page() {
-  const {
-    data: terms,
-    isLoading: termsLoading,
-    error: termsError,
-  } = trpc.getTerms.useQuery();
-
-  const selectedSessionsURL = useSelectedSessionsURL();
-  const selectedTerm = useSelectedTerm();
-  const { setInitialData } = useScheduleActions();
-
-  const [selected, setSelected] = useQueryState("data", {
-    defaultValue: null,
-    history: "replace",
-    parse: value => JSON.parse(LZString.decompressFromBase64(value)),
-    serialize: value => LZString.compressToBase64(JSON.stringify(value)),
-  });
-  const [term, setTerm] = useQueryState("term", {
-    history: "replace",
-  });
-
-  if (terms) {
-    const initialTerm = terms.find(termData => termData.value === term);
-    const selectedTerm = initialTerm ? initialTerm : (terms[0] as Term);
-
-    if (!initialTerm || !selected) {
-      setInitialData({
-        courseSearchResults: [],
-        selectedSessionsURL: null,
-        selectedTerm,
-        availableTerms: terms,
-      });
-    }
-  }
-
-  const hasInitialized = useRef(false);
-
-  useEffect(() => {
-    if (hasInitialized.current) return;
-    hasInitialized.current = true;
-
-    if (!terms) return;
-
-    const fetchInitialData = async () => {
-      const toFetch = Object.keys(selected).map(courseCode =>
-        fetchCourse(courseCode, selectedTerm),
-      );
-      const results = await Promise.allSettled(toFetch);
-
-      if (results.some(result => result.status === "rejected")) {
-        setInitialData({
-          courseSearchResults: [],
-          selectedSessionsURL: null,
-          selectedTerm: selectedTerm,
-          availableTerms: terms,
-        });
-        return;
-      }
-
-      const courses = results
-        .filter(result => result.status !== "rejected")
-        .map(result => result.value);
-
-      setInitialData({
-        courseSearchResults: courses,
-        selectedSessionsURL: selected,
-        selectedTerm,
-        availableTerms: terms,
-      });
-    };
-    fetchInitialData();
-  }, [selected, setInitialData, term]);
-
-  // Sync reducer state back to URL whenever data changes
-  useEffect(() => {
-    setSelected(selectedSessionsURL);
-  }, [selectedSessionsURL, setSelected]);
-
-  useEffect(() => {
-    setTerm(selectedTerm ? selectedTerm.value : null);
-  }, [selectedTerm, setTerm]);
-
   return (
     <App>
       <Sidebar>
