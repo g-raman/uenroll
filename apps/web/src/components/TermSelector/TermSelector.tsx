12fd9e1c503c72f5ef71c33edb2f9377f3415e0e
Raman Gupta <gr.gupta.raman@gmail.com>
2025-07-09 13:28:39 -0400

    feat (web): use trpc for term selector

--- apps/web/src/components/TermSelector/TermSelector.tsx
+++ apps/web/src/components/TermSelector/TermSelector.tsx
@@ -1,11 +1,8 @@
 "use client";
 
-import {
-  useAvailableTerms,
-  useScheduleActions,
-  useSelectedTerm,
-} from "@/stores/scheduleStore";
-import { Term } from "@/types/Types";
+import { trpc } from "@/app/_trpc/client";
+import { useSelectedSessionsURL } from "@/hooks/useSelectedSessionsURL";
+import { useTerm } from "@/hooks/useTerm";
 import {
   Select,
   SelectContent,
@@ -14,34 +11,32 @@ import {
   SelectValue,
 } from "@repo/ui/components/select";
 import { Skeleton } from "@repo/ui/components/skeleton";
+import { useQuery } from "@tanstack/react-query";
 
 export default function TermSelector() {
-  const availableTerms = useAvailableTerms();
-  const selectedTerm = useSelectedTerm();
-  const { changeTerm } = useScheduleActions();
+  const [selectedTerm, setSelectedTerm] = useTerm();
+  const [, setSelectedSessions] = useSelectedSessionsURL();
+  const { data: availableTerms } = useQuery(trpc.getTerms.queryOptions());
 
-  function handleSelect(event: string) {
-    const term = JSON.parse(event) as Term;
-    changeTerm(term);
+  function handleChangeTerm(term: string) {
+    setSelectedTerm(term);
+    setSelectedSessions(null);
   }
 
   return (
     <>
-      {availableTerms.length === 0 ? (
+      {!availableTerms || availableTerms.length === 0 ? (
         <Skeleton className="h-8 w-full" />
       ) : (
-        <Select
-          defaultValue={selectedTerm ? JSON.stringify(selectedTerm) : ""}
-          onValueChange={handleSelect}
-        >
+        <Select defaultValue={selectedTerm} onValueChange={handleChangeTerm}>
           <SelectTrigger className="w-full">
             <SelectValue placeholder="Term" />
           </SelectTrigger>
 
           <SelectContent>
-            {availableTerms.map(elem => (
-              <SelectItem key={elem.value} value={JSON.stringify(elem)}>
-                {elem.term}
+            {availableTerms.map(availableTerm => (
+              <SelectItem key={availableTerm.value} value={availableTerm.value}>
+                {availableTerm.term}
               </SelectItem>
             ))}
           </SelectContent>
