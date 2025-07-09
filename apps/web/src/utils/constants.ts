caedb8ff94511974bcdf8b182c62280f83aa2243
Raman Gupta <gr.gupta.raman@gmail.com>
2025-07-09 18:00:59 -0400

    feat (web): add constant for course query stale time and gc time

--- apps/web/src/utils/constants.ts
+++ apps/web/src/utils/constants.ts
@@ -29,6 +29,9 @@ export const BG_COLOURS = [
 
 export const MAX_RESULTS_ALLOWED = INITIAL_COLOURS.length;
 
+export const COURSE_STALE_TIME = 1000 * 60 * 5; // 5 minutes
+export const COURSE_GC_TIME = 1000 * 60 * 10; // 10 minutes
+
 export const dayOfWeekToNumberMap: { [key: string]: number } = {
   "N/A": -1,
   Mo: 1,
