19127682733e2c1efb292848f29234b9b916b712
Raman Gupta <gr.gupta.raman@gmail.com>
2025-07-09 17:24:22 -0400

    refactor (web): used new hooks for download calendar button

--- apps/web/src/components/Buttons/DownloadCalendarButton/DownloadCalendarButton.tsx
+++ apps/web/src/components/Buttons/DownloadCalendarButton/DownloadCalendarButton.tsx
@@ -1,5 +1,7 @@
-import { useSelectedSessions } from "@/stores/scheduleStore";
-import { getCalendar } from "@/utils/generateICS";
+import { useCourseQueries } from "@/hooks/useCourseQueries";
+import { useSelectedSessionsURL } from "@/hooks/useSelectedSessionsURL";
+import { useTermParam } from "@/hooks/useTermParam";
+import { createDownloadableCalendarEvents } from "@/utils/calendarEvents";
 import { faFileExport } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "@repo/ui/components/button";
@@ -9,13 +11,38 @@ import {
   TooltipTrigger,
 } from "@repo/ui/components/tooltip";
 import React from "react";
+import { generateIcsCalendar } from "ts-ics";
 
 export default function DownloadCalendarButton() {
   const filename = "schedule.ics";
-  const selectedSessions = useSelectedSessions();
+  const [selectedTerm] = useTermParam();
+  const [selected] = useSelectedSessionsURL();
+  const courseCodes = Object.keys(selected ? selected : {});
+  const courseQueries = useCourseQueries(
+    selectedTerm,
+    courseCodes,
+    courseCodes.length >= 0,
+  );
+
+  const hasAnySelectedSessions = Object.values(selected ? selected : {}).some(
+    value => value.length > 0,
+  );
+
+  const courseSearchResults = courseQueries
+    .filter(query => query.isSuccess)
+    .map(query => query.data);
 
   async function handleDownload() {
-    const calendar = getCalendar(selectedSessions);
+    const events = createDownloadableCalendarEvents(
+      courseSearchResults,
+      selected,
+    );
+    const calendar = generateIcsCalendar({
+      version: "2.0",
+      prodId: "//uEnroll//Calendar Export 1.0//EN",
+      events,
+    });
+
     const blob = new Blob([calendar], { type: "text/calendar" });
     const url = URL.createObjectURL(blob);
 
@@ -38,7 +65,7 @@ export default function DownloadCalendarButton() {
           variant="default"
           size="lg"
           onClick={handleDownload}
-          disabled={selectedSessions.length === 0}
+          disabled={!hasAnySelectedSessions}
         >
           <FontAwesomeIcon className="size-4" icon={faFileExport} />
           <p className="hidden text-xs min-[375px]:inline sm:inline md:hidden min-[1440px]:inline">
