070aff2dd657a6db555fed9e0ebc07652b5c0f81
Raman Gupta <gr.gupta.raman@gmail.com>
2025-07-10 13:24:01 -0400

    Fix/base url (#112)
    * fix (web): use window object to get base url for trpc api to avoid cors on non-production branches
    
    * chore: add changeset

--- apps/web/src/app/_trpc/client.ts
+++ apps/web/src/app/_trpc/client.ts
@@ -5,8 +5,17 @@ import { createTRPCOptionsProxy } from "@trpc/tanstack-react-query";
 import { envClient } from "@repo/env";
 
 export const queryClient = new QueryClient();
+
+let [host, protcol] = ["", ""];
+if (typeof window !== "undefined") {
+  protcol = window.location.protocol;
+  host = window.location.host;
+}
+const baseUrl =
+  !protcol || !host ? envClient.NEXT_PUBLIC_BASE_URL : `${protcol}//${host}`;
+
 const trpcClient = createTRPCClient<AppRouter>({
-  links: [httpBatchLink({ url: `${envClient.NEXT_PUBLIC_BASE_URL}/api/trpc` })],
+  links: [httpBatchLink({ url: `${baseUrl}/api/trpc` })],
 });
 
 export const trpc = createTRPCOptionsProxy<AppRouter>({
