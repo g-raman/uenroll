c80b1cd3ed29654711427f93937685a12db7a39d
Raman Gupta <gr.gupta.raman@gmail.com>
2025-10-02 14:43:41 -0400

    refactor (web): move course utils from mappers to separate utils file

---
+++ apps/web/src/utils/course.ts
@@ -0,0 +1,42 @@
+import { GroupedSearchResults } from "@/types/Types";
+import { Section } from "@repo/db/types";
+
+export const isAlternativeSubSection = (first: Section, second: Section) => {
+  if (
+    first.sessions.length !== second.sessions.length ||
+    first.type !== second.type ||
+    first.subSection === second.subSection
+  )
+    return false;
+
+  return first.sessions.every(fSession =>
+    second.sessions.some(
+      sSession =>
+        fSession.startTime === sSession.startTime &&
+        fSession.endTime === sSession.endTime &&
+        fSession.startDate === sSession.startDate &&
+        fSession.endDate === sSession.endDate &&
+        fSession.dayOfWeek === sSession.dayOfWeek,
+    ),
+  );
+};
+
+export const sortCoursesByNumSubSections = (
+  courses: GroupedSearchResults[],
+) => {
+  courses.sort(
+    (a, b) =>
+      Object.values(a.sections).reduce(
+        (prev, curr) =>
+          prev +
+          curr.reduce((prev2, curr2) => prev2 + curr2.sessions.length, 0),
+        0,
+      ) -
+      Object.values(b.sections).reduce(
+        (prev, curr) =>
+          prev +
+          curr.reduce((prev2, curr2) => prev2 + curr2.sessions.length, 0),
+        0,
+      ),
+  );
+};
