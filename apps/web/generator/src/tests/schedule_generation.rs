6ec5d44aa932c9fd8fcae58fc826ee3a15edd735
Raman Gupta <gr.gupta.raman@gmail.com>
2025-10-17 15:34:59 -0400

    fix (web): add simple test for checking for time conflict

--- apps/web/generator/src/tests/schedule_generation.rs
+++ apps/web/generator/src/tests/schedule_generation.rs
@@ -1,5 +1,6 @@
+use crate::datetime::is_overlapping_time;
 use crate::generator::generate_schedules;
-use crate::types::CourseWithSectionAlternatives;
+use crate::types::{CourseWithSectionAlternatives, Session};
 use std::fs;
 
 #[test]
@@ -9,4 +10,29 @@ fn test_generate_schedule() {
     let schedules = generate_schedules(&[course]);
     println!("Found {} possible schedules", schedules.len());
     assert!(!schedules.is_empty());
+}
+
+#[test]
+fn test_has_conflict() {
+    println!(
+        "{}",
+        is_overlapping_time(
+            &Session {
+                start_date: "2025-09-03".to_string(),
+                start_time: "11:30:00".to_string(),
+                end_time: "12:50:00".to_string(),
+                end_date: "2025-12-02".to_string(),
+                day_of_week: "Th".to_string(),
+                instructor: "test".to_string(),
+            },
+            &Session {
+                start_date: "2025-12-18".to_string(),
+                start_time: "10:00:00".to_string(),
+                end_time: "12:50:00".to_string(),
+                end_date: "2025-12-18".to_string(),
+                day_of_week: "Th".to_string(),
+                instructor: "test".to_string(),
+            },
+        )
+    );
 }
