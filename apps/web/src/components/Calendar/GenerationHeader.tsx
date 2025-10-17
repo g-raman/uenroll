7345573b7f5162a7da6cf6699fd2ae853328eb2d
Raman Gupta <gr.gupta.raman@gmail.com>
2025-10-17 18:43:51 -0400

    tweak (web): align logo and text vertically

--- apps/web/src/components/Calendar/GenerationHeader.tsx
+++ apps/web/src/components/Calendar/GenerationHeader.tsx
@@ -139,7 +139,7 @@ export function GenerationHeader() {
 
   return (
     <div className="bg-background sticky top-0 z-10 flex items-center justify-between rounded-md border px-4 py-3">
-      <div className="flex items-baseline justify-start text-4xl">
+      <div className="flex items-center justify-start text-4xl">
         <FontAwesomeIcon
           className="text-primary size-12"
           icon={faBuildingColumns}
