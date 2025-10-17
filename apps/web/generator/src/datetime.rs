9c24276cee52894c5b3c6851f93148f7555c22a6
Raman Gupta <gr.gupta.raman@gmail.com>
2025-10-17 15:33:48 -0400

    refactor (web): simplify logic for checking for overlaps

--- apps/web/generator/src/datetime.rs
+++ apps/web/generator/src/datetime.rs
@@ -1,60 +1,32 @@
-use time::{Date, Duration, OffsetDateTime, PrimitiveDateTime, Time};
-use time_tz::{PrimitiveDateTimeExt, Tz, timezones};
+use time::Time;
 
 use crate::types::Session;
 
-fn toronto_tz() -> &'static Tz {
-    timezones::get_by_name("America/Toronto").expect("America/Toronto timezone not found")
-}
-
-pub fn get_zoned_datetime(date: &str, time_str: &str) -> OffsetDateTime {
-    let date = Date::parse(
-        date,
-        &time::format_description::well_known::Iso8601::DEFAULT,
-    )
-    .expect("Invalid date format");
-
-    let time = Time::parse(
-        time_str,
-        &time::format_description::well_known::Iso8601::DEFAULT,
-    )
-    .expect("Invalid time string");
-    let naive = PrimitiveDateTime::new(date, time);
-
-    naive.assume_timezone_utc(toronto_tz())
-}
-
-pub fn get_offsetted_start_datetime(date: &str, time: &str, day_of_week: &str) -> OffsetDateTime {
-    let base = get_zoned_datetime(date, time);
-    let base_day = base.weekday().number_days_from_monday() as i64;
-    let target_day = day_of_week_to_number_map(day_of_week);
-    let offset_days = (target_day - base_day + 7) % 7;
-    base + Duration::days(offset_days)
-}
-
 pub fn is_overlapping_time(first: &Session, second: &Session) -> bool {
-    let f_start =
-        get_offsetted_start_datetime(&first.start_date, &first.start_time, &first.day_of_week);
-    let f_end =
-        get_offsetted_start_datetime(&first.start_date, &first.end_time, &first.day_of_week);
-    let s_start =
-        get_offsetted_start_datetime(&second.start_date, &second.start_time, &second.day_of_week);
-    let s_end =
-        get_offsetted_start_datetime(&second.start_date, &second.end_time, &second.day_of_week);
+    if first.day_of_week != second.day_of_week {
+        return false;
+    }
+
+    let f_start = Time::parse(
+        &first.start_time,
+        &time::format_description::well_known::Iso8601::DEFAULT,
+    )
+    .unwrap();
+    let f_end = Time::parse(
+        &first.end_time,
+        &time::format_description::well_known::Iso8601::DEFAULT,
+    )
+    .unwrap();
+    let s_start = Time::parse(
+        &second.start_time,
+        &time::format_description::well_known::Iso8601::DEFAULT,
+    )
+    .unwrap();
+    let s_end = Time::parse(
+        &second.end_time,
+        &time::format_description::well_known::Iso8601::DEFAULT,
+    )
+    .unwrap();
 
     f_start < s_end && s_start < f_end
-}
-
-fn day_of_week_to_number_map(day: &str) -> i64 {
-    match day {
-        "N/A" => -1,
-        "Mo" => 1,
-        "Tu" => 2,
-        "We" => 3,
-        "Th" => 4,
-        "Fr" => 5,
-        "Sa" => 6,
-        "Su" => 7,
-        _ => panic!("Invalid weekday: {}", day),
-    }
 }
