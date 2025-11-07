5734f9fdeb7d0716777deb13686a03cd747d9401
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-07 10:47:24 -0500

    feat (web): make delta for next and previous button dynamic for header

--- apps/web/src/components/Calendar/CalendarHeader/CalendarHeader.tsx
+++ apps/web/src/components/Calendar/CalendarHeader/CalendarHeader.tsx
@@ -1,3 +1,5 @@
+"use no memo";
+
 import { CopyLinkButton } from "@/components/Buttons/CopyLinkButton/CopyLinkButton";
 import DownloadCalendarButton from "@/components/Buttons/DownloadCalendarButton/DownloadCalendarButton";
 import { useTermParam } from "@/hooks/useTermParam";
@@ -9,6 +11,7 @@ import {
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { Button } from "@repo/ui/components/button";
 import { CalendarConfig } from "@schedule-x/calendar";
+import { useCallback } from "react";
 
 export interface App {
   config: CalendarConfig & {
@@ -20,12 +23,18 @@ export interface App {
           end: Temporal.ZonedDateTime;
         };
         setDate: (date: Temporal.PlainDate) => void;
+        setFirstDayOfWeek: (firstDayOfWeek: number) => void;
       };
     };
   };
 }
 
-export default function CalendarHeader({ $app }: { $app: App }) {
+interface CalendarHeaderProps {
+  $app: App;
+  delta: number;
+}
+
+export default function CalendarHeader({ $app, delta }: CalendarHeaderProps) {
   const [term] = useTermParam();
 
   const date = $app.config.plugins.calendarControls.getDate();
@@ -40,9 +49,20 @@ export default function CalendarHeader({ $app }: { $app: App }) {
     month: "long",
   });
 
+  const handleNextOrPrevious = useCallback(
+    (change: number) => {
+      const newDate = date.add({ days: change });
+      const dayOfWeek = delta === 7 ? 1 : newDate.dayOfWeek;
+
+      $app.config.plugins.calendarControls.setDate(newDate);
+      $app.config.plugins.calendarControls.setFirstDayOfWeek(dayOfWeek);
+    },
+    [$app.config.plugins.calendarControls, date, delta],
+  );
+
   return (
     <>
-      <div className="hidden h-full w-full items-center gap-4 lg:flex">
+      <div className="flex h-full w-full flex-wrap items-center gap-4">
         <Button
           className="bg-background! shadow-xs! hover:bg-accent! hover:text-accent-foreground! dark:bg-input/30! dark:border-input! dark:hover:bg-input/50! border text-black dark:text-white"
           variant="default"
@@ -69,28 +89,19 @@ export default function CalendarHeader({ $app }: { $app: App }) {
           <DownloadCalendarButton />
           <Button
             className="bg-background! shadow-xs! hover:bg-accent! hover:text-accent-foreground! dark:bg-input/30! dark:border-input! dark:hover:bg-input/50! border text-black dark:text-white"
-            onClick={() =>
-              $app.config.plugins.calendarControls.setDate(
-                date.add({ days: -7 }),
-              )
-            }
+            onClick={() => handleNextOrPrevious(delta * -1)}
           >
             <FontAwesomeIcon icon={faChevronLeft} />
           </Button>
 
           <Button
             className="bg-background! shadow-xs! hover:bg-accent! hover:text-accent-foreground! dark:bg-input/30! dark:border-input! dark:hover:bg-input/50! border text-black dark:text-white"
-            onClick={() =>
-              $app.config.plugins.calendarControls.setDate(
-                date.add({ days: 7 }),
-              )
-            }
+            onClick={() => handleNextOrPrevious(delta)}
           >
             <FontAwesomeIcon icon={faChevronRight} />
           </Button>
         </div>
       </div>
-      <p className="text-lg lg:hidden">Schedule</p>
     </>
   );
 }
