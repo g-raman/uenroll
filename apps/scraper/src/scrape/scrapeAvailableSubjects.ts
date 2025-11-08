1cf51c62c68e01652dfa61d14fb111d78832452b
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-08 17:58:45 -0500

    feat (scraper): add new scrape available subjects method using uottawa website instead

---
+++ apps/scraper/src/scrape/scrapeAvailableSubjects.ts
@@ -0,0 +1,48 @@
+import { updateAvailableSubjects } from "@repo/db/queries";
+import type { SubjectInsert } from "@repo/db/types";
+import * as cheerio from "cheerio";
+
+const catalogue = await fetch("https://catalogue.uottawa.ca/en/courses/");
+const html = await catalogue.text();
+const $ = cheerio.load(html);
+
+const SUBJECT_CODE_REGEX = /\(([^()]*)\)(?!.*\([^()]*\))/;
+const subjectCodeRegex = new RegExp(SUBJECT_CODE_REGEX);
+
+const subjects: SubjectInsert[] = [];
+$(".letternav-head + ul > li").each(function () {
+  const subject = $(this).text();
+  const isMatch = subjectCodeRegex.test(subject);
+  if (!isMatch) {
+    console.log(`Coudln't find a match for ${subject}`);
+    return;
+  }
+
+  const subjectCode = subjectCodeRegex.exec(subject);
+  if (!subjectCode || !subjectCode[1]) {
+    console.log(`Something went wrong trying to match for subject: ${subject}`);
+    return;
+  }
+
+  subjects.push({ subject: subjectCode[1] });
+});
+
+// Manual entries for courses that aren't listed in the catalogue
+subjects.push({ subject: "PBH" });
+subjects.push({ subject: "ADX" });
+subjects.push({ subject: "CTM" });
+subjects.push({ subject: "DTO" });
+subjects.push({ subject: "EHA" });
+subjects.push({ subject: "EMC" });
+subjects.push({ subject: "ESG" });
+subjects.push({ subject: "MEM" });
+subjects.push({ subject: "MIA" });
+subjects.push({ subject: "POP" });
+subjects.push({ subject: "PHM" });
+
+const result = await updateAvailableSubjects(subjects);
+if (result.isErr()) {
+  console.log(result.error);
+  process.exit(1);
+}
+process.exit(0);
