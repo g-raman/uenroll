f4e6fd7c2068b15c16cb7b1c4125a18ce46ba31d
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-14 14:03:59 -0500

    feat: source correct file

--- apps/scraper/terraform/setup.sh
+++ apps/scraper/terraform/setup.sh
@@ -13,7 +13,7 @@ sudo dnf install -y git curl unzip --allowerasing
 
 # Install Node
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
-source /.nvm/nvm.sh
+source "$WORK_DIR/.nvm/nvm.sh"
 nvm install 24
 
 # Install Bun
