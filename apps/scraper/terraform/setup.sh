9309fe685de7a37bc66040698e7cf35523a5ce66
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-14 13:58:06 -0500

    fix: use work dir

--- apps/scraper/terraform/setup.sh
+++ apps/scraper/terraform/setup.sh
@@ -3,6 +3,7 @@
 set -eo pipefail
 
 export HOME="$${HOME:-/home/ec2-user}"
+WORK_DIR="/home/ec2-user"
 
 # Update system
 sudo dnf update -y
@@ -12,24 +13,24 @@ sudo dnf install -y git curl unzip --allowerasing
 
 # Install Bun
 curl -fsSL https://bun.com/install | bash -s "bun-v1.3.2"
-source "$HOME/.bash_profile"
+source "$WORK_DIR/.bash_profile"
 
 # Clone repo
-WORK_DIR="/uenroll"
 git clone https://github.com/g-raman/uenroll.git "$WORK_DIR"
 
 # Add API key to .env
-echo "DATABASE_URL=\"${DATABASE_URL}\"" >> "/uenroll/apps/scraper/.env"
+echo "DATABASE_URL=\"${DATABASE_URL}\"" >> "$WORK_DIR/uenroll/apps/scraper/.env"
 
 # Install dependencies
-cd "$WORK_DIR"
-"$HOME/.bun/bin/bun" install --filter scraper
+cd "$WORK_DIR/uenroll"
+"$WORK_DIR/.bun/bin/bun" install --filter scraper
 
 # Install Turbo
-"$HOME/.bun/bin/bun" install -g turbo
+"$WORK_DIR/.bun/bin/bun" install -g turbo
+source "$WORK_DIR/.bash_profile"
 
 # Build Scraper
-"$HOME/.bun/bin/bun" run build --filter scraper
+"$WORK_DIR/.bun/bin/bun" run build --filter scraper
 
 # Make director to store logs
 mkdir -p /var/logs/scraper/
@@ -38,7 +39,6 @@ mkdir -p /var/logs/scraper/
 echo 'export GITHUB_TOKEN="${GITHUB_TOKEN}"' >> /etc/profile
 source /etc/profile
 
-WORK_DIR="/uenroll/apps/scraper"
-cd "$WORK_DIR"
+cd "$WORK_DIR/uenroll/apps/scraper"
 chmod +x ./src/scrape.sh
 # ./src/scrape.sh >> /var/logs/scraper/scraper.log
