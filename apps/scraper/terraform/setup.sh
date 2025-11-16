ab4c5293fc68f29b930f8d40643fc4a1fbb755e1
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-16 18:37:25 -0500

    fix: use npm to install bun

--- apps/scraper/terraform/setup.sh
+++ apps/scraper/terraform/setup.sh
@@ -17,8 +17,7 @@ source "$WORK_DIR/.nvm/nvm.sh"
 nvm install 24
 
 # Install Bun
-curl -fsSL https://bun.com/install | bash -s "bun-v1.3.2"
-source "$WORK_DIR/.bash_profile"
+npm install -g bun@1.3.2
 
 # Clone repo
 git clone https://github.com/g-raman/uenroll.git "$WORK_DIR/uenroll"
@@ -28,14 +27,14 @@ echo "DATABASE_URL=\"${DATABASE_URL}\"" >> "$WORK_DIR/uenroll/apps/scraper/.env"
 
 # Install dependencies
 cd "$WORK_DIR/uenroll"
-"$WORK_DIR/.bun/bin/bun" install --filter scraper
+bun install --filter scraper
 
 # Install Turbo
-"$WORK_DIR/.bun/bin/bun" install -g turbo
+bun install -g turbo
 source "$WORK_DIR/.bash_profile"
 
 # Build Scraper
-"$WORK_DIR/.bun/bin/bun" run build --filter scraper
+bun run build --filter scraper
 
 # Make director to store logs
 mkdir -p /var/logs/scraper/
