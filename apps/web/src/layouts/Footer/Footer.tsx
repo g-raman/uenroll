0c6d837307d308bf27daf4ae783e2605bbc1dad1
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-16 16:21:16 -0500

    fix (web): footer colours

--- apps/web/src/layouts/Footer/Footer.tsx
+++ apps/web/src/layouts/Footer/Footer.tsx
@@ -6,7 +6,11 @@ export const Footer = () => {
     <div className="mt-auto hidden text-center text-sm md:block">
       <p className="inline-block">Mantained by</p>
       &nbsp;
-      <Button className="p-0 underline dark:text-white" variant="link" asChild>
+      <Button
+        className="text-foreground p-0 underline hover:opacity-70"
+        variant="link"
+        asChild
+      >
         <a
           href="https://www.linkedin.com/in/gupta-raman/"
           target="_blank"
@@ -16,7 +20,11 @@ export const Footer = () => {
         </a>
       </Button>
       ,&nbsp;
-      <Button className="p-0 underline dark:text-white" variant="link" asChild>
+      <Button
+        className="text-foreground p-0 underline hover:opacity-70"
+        variant="link"
+        asChild
+      >
         <a
           href="https://github.com/g-raman/uenroll"
           target="_blank"
