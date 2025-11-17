50c52232c2183c97fbd34aa15a290fb594a83540
Raman Gupta <gr.gupta.raman@gmail.com>
2025-11-16 21:17:26 -0500

    feat (web + ui): run posthog wizard and set env vars

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -6,6 +6,19 @@ const nextConfig: NextConfig = {
     ignoreBuildErrors: true,
   },
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/ingest/static/:path*",
+        destination: "https://us-assets.i.posthog.com/static/:path*",
+      },
+      {
+        source: "/ingest/:path*",
+        destination: "https://us.i.posthog.com/:path*",
+      },
+    ];
+  },
+  skipTrailingSlashRedirect: true,
 };
 
 export default nextConfig;
