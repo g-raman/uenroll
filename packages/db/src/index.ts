7424d5ff753d8d7127de25713a5957b0f1f7529d
Raman Gupta <gr.gupta.raman@gmail.com>
2026-01-15 17:18:12 -0500

    fix (db): import env from server package

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
 import * as schema from "./schema.js";
-import { env } from "@repo/env";
+import { env } from "@repo/env/server";
 
 export const client = postgres(env.DATABASE_URL, { prepare: false });
 export const db = drizzle(client, { schema, casing: "snake_case" });
