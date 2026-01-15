5dab4af889ba9658669af9b23843c155ea4fd5cc
Raman Gupta <gr.gupta.raman@gmail.com>
2026-01-15 17:14:55 -0500

    feat (env): add separate entrypoints for server and client variables

--- packages/env/tsup.config.ts
+++ packages/env/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
+  entry: ["src/index.ts", "src/server.ts", "src/client.ts"],
   outDir: "dist",
   // Both ESM & CJS for Next.js compatibility
   format: ["esm", "cjs"],
