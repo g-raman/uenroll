aeaab08d0ec3a3a419624a65442e4f9064c862a6
Raman Gupta <gr.gupta.raman@gmail.com>
2026-01-27 20:20:26 -0500

    feat (web): make layout responsive

--- apps/web/src/layouts/Main/Main.tsx
+++ apps/web/src/layouts/Main/Main.tsx
@@ -2,7 +2,7 @@ export default function Main({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="h-full overflow-hidden rounded-t-md border-none bg-white md:h-full md:rounded-md dark:bg-[#141218]">
+    <div className="h-full min-h-[48rem] w-full overflow-hidden rounded-t-md border-none bg-white md:h-full md:rounded-md dark:bg-[#141218]">
       {children}
     </div>
   );
--- apps/web/src/layouts/Sidebar/Sidebar.tsx
+++ apps/web/src/layouts/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ export default function Sidebar({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="bg-background flex h-[calc(100%-0.5rem)] w-full flex-col overflow-x-scroll rounded-b-md px-4 pb-4 md:order-last md:h-full md:w-[calc(100%-1rem)] md:rounded-md">
+    <div className="bg-background flex h-[calc(100%-0.5rem)] flex-col rounded-b-md px-4 pb-4 md:rounded-md lg:order-last lg:h-full lg:max-w-[30%]">
       {children}
       <Footer />
     </div>
