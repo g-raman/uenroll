6e77f005e0cfa2f5e8efa17686057941334d7042
Raman Gupta <gr.gupta.raman@gmail.com>
2026-01-27 21:37:43 -0500

    fix (web): sidebar width

--- apps/web/src/layouts/Sidebar/Sidebar.tsx
+++ apps/web/src/layouts/Sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ export default function Sidebar({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="bg-background flex flex-col rounded-b-md px-4 pb-4 md:h-[calc(100%-0.5rem)] md:rounded-md lg:order-last lg:h-full lg:max-w-[30%]">
+    <div className="bg-background flex flex-col rounded-b-md px-4 pb-4 md:h-[calc(100%-0.5rem)] md:rounded-md lg:order-last lg:h-full lg:max-w-[30%] lg:min-w-[30%]">
       {children}
       <Footer />
     </div>
