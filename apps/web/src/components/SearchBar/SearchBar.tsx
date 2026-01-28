8fcff9fb72e8271bca546aed47aee85ea3830b6b
Raman Gupta <gr.gupta.raman@gmail.com>
2026-01-28 16:43:10 -0500

    cleanup (web + ui): remove autocomplete

--- apps/web/src/components/SearchBar/SearchBar.tsx
+++ apps/web/src/components/SearchBar/SearchBar.tsx
@@ -1,14 +1,11 @@
 import TermSelector from "../TermSelector/TermSelector";
 import { DeleteSearchResultsButton } from "../Buttons/DeleteSearchResultsButton/DeleteSearchResultsButton";
 import { ThemeSwitchingButton } from "../Buttons/ThemeSwitchingButton/ThemeSwitchingButton";
-import Autocomplete from "./Autocomplete";
 
 export default function SearchBar() {
   return (
     <div className="bg-background sticky top-0 z-10 flex flex-col gap-2 py-4">
       <TermSelector />
-
-      <Autocomplete />
 
       <div className="flex gap-2">
         <DeleteSearchResultsButton />
