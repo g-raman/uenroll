1168a9cfdc5f3ee21833022c8351eb3c0039b3d7
Raman Gupta <gr.gupta.raman@gmail.com>
2026-05-09 06:44:18 -0400

    feat (web): add vite config

---
+++ apps/web/vite.config.ts
@@ -0,0 +1,40 @@
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
+import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
+import { defineConfig, loadEnv } from "vite";
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    define: {
+      "process.env.NEXT_PUBLIC_BASE_URL": JSON.stringify(
+        env.NEXT_PUBLIC_BASE_URL,
+      ),
+      "process.env.NEXT_PUBLIC_POSTHOG_HOST": JSON.stringify(
+        env.NEXT_PUBLIC_POSTHOG_HOST,
+      ),
+      "process.env.NEXT_PUBLIC_POSTHOG_KEY": JSON.stringify(
+        env.NEXT_PUBLIC_POSTHOG_KEY,
+      ),
+    },
+    server: {
+      port: 3000,
+    },
+    resolve: {
+      tsconfigPaths: true,
+    },
+    plugins: [
+      tailwindcss(),
+      tanstackStart({
+        srcDirectory: "src",
+        router: {
+          routesDirectory: "app",
+        },
+      }),
+      viteReact(),
+      nitro(),
+    ],
+  };
+});
